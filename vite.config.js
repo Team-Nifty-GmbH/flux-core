515eff978182a61f3db0d94640da68416aa35dbe
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-05-10 12:27:54 +0200

    add apex charts

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/apex-charts.js',
                 'resources/js/alpine.js',
             ],
             refresh: false
