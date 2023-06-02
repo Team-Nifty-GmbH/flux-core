1103d9877bae4b5a655d96143f50705e937c7ce5
Steffen Franz <der.wichtel@yahoo.de>
2023-06-02 15:20:13 +0200

    refactor discounts table

--- src/Http/Requests/CreateDiscountRequest.php
+++ src/Http/Requests/CreateDiscountRequest.php
@@ -2,6 +2,10 @@
 
 namespace FluxErp\Http\Requests;
 
+use FluxErp\Rules\ClassExists;
+use FluxErp\Rules\MorphExists;
+use Illuminate\Database\Eloquent\Model;
+
 class CreateDiscountRequest extends BaseFormRequest
 {
     /**
@@ -12,9 +16,20 @@ class CreateDiscountRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'sort_number' => 'integer|min:0',
-            'order_position_id' => 'required|integer|exists:order_positions,id,deleted_at,NULL',
+            'model_type' => [
+                'required_with:model_id',
+                'string',
+                new ClassExists(instanceOf: Model::class),
+            ],
+            'model_id' => [
+                'required_with:model_type',
+                'integer',
+                new MorphExists(),
+            ],
             'discount' => 'required|numeric',
+            'from' => 'nullable|date_format:Y-m-d H:i:s',
+            'till' => 'nullable|date_format:Y-m-d H:i:s',
+            'sort_number' => 'integer|min:0',
             'is_percentage' => 'required|boolean',
         ];
     }
