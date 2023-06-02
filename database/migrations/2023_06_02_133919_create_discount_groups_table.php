5b77ce17ea6f253358d4128444dc51acc1e2341e
Steffen Franz <der.wichtel@yahoo.de>
2023-06-02 16:24:28 +0200

    remove unnecessary columns

--- database/migrations/2023_06_02_133919_create_discount_groups_table.php
+++ database/migrations/2023_06_02_133919_create_discount_groups_table.php
@@ -15,13 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->boolean('is_active')->default(true);
-            $table->timestamp('created_at')->nullable();
-            $table->unsignedBigInteger('created_by')->nullable();
-            $table->timestamp('updated_at')->nullable();
-            $table->unsignedBigInteger('updated_by')->nullable();
-
-            $table->foreign('created_by')->references('id')->on('users');
-            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 
