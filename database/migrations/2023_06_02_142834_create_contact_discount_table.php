5417ebf6b99d45438cdd814831b281b3eaa0310c
Steffen Franz <der.wichtel@yahoo.de>
2023-06-02 16:34:34 +0200

    add contact_discount pivot table

---
+++ database/migrations/2023_06_02_142834_create_contact_discount_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_discount', function (Blueprint $table) {
+            $table->unsignedBigInteger('contact_id');
+            $table->unsignedBigInteger('discount_id');
+
+            $table->primary(['contact_id', 'discount_id']);
+
+            $table->foreign('contact_id')
+                ->references('id')
+                ->on('contacts')
+                ->cascadeOnDelete();
+            $table->foreign('discount_id')
+                ->references('id')
+                ->on('discounts')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_discount');
+    }
+};
