5d36a0594d0a2f1494c26e4b07f268cb9be19e27
Steffen Franz <der.wichtel@yahoo.de>
2023-06-05 13:32:49 +0200

    add discount group controller and service

---
+++ src/Http/Requests/UpdateDiscountGroupRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FluxErp\Http\Requests;
+
+class UpdateDiscountGroupRequest extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|integer|exists:discount_groups,id',
+            'name' => 'sometimes|required|string',
+            'is_active' => 'boolean',
+        ];
+    }
+}
