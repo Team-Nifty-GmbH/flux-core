c9680c616c98d3a560091f39026228398afc9439
benefarr <farr.benedikt256@gmail.com>
2023-06-16 09:47:12 +0200

    Added Total profit widget

---
+++ resources/views/livewire/widgets/total-profit.blade.php
@@ -0,0 +1,18 @@
+<x-card class="max-w-full" x-data="{total: $wire.entangle('sum')}">
+    <div class="flex text-ellipsis overflow-hidden flex-col items-start overflow-auto">
+        <x-button.circle xl class="cursor-default" icon="credit-card" orange></x-button.circle>
+        <p class="truncate text-xl font-semibold pt-3">{{__('Total profits')}}</p>
+        <div class="flex flex-col items-start pt-3">
+            <p class="truncate text-md font-medium pt-1">{{__($selectedTimeFrame)}}</p>
+            <p class="text-lg font-normal text-gray-500" x-text="formatters.money(total, @js($currency))"></p>
+        </div>
+    </div>
+    <x-slot:footer>
+        <x-select
+            label="Select Time Frame"
+            placeholder="Select a time frame"
+            :options="$timeFrames"
+            wire:model="timeFrame"
+        />
+    </x-slot:footer>
+</x-card>
---
+++ src/Http/Livewire/Widgets/TotalProfit.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace FluxErp\Http\Livewire\Widgets;
+
+use FluxErp\Contracts\UserWidget;
+use FluxErp\Enums\TimeFrameEnum;
+use FluxErp\Models\Currency;
+use FluxErp\Models\Order;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Livewire\Component;
+
+class TotalProfit extends Component implements UserWidget
+{
+    public float $sum = 0;
+
+    public string $timeFrame = 'Last Month';
+
+    public array $timeFrames = [];
+
+    protected $listeners = ['timeFrameUpdated' => 'updateTimeFrame'];
+
+    public function mount(): void
+    {
+        $this->timeFrames = TimeFrameEnum::values();
+        $this->calculateSum();
+    }
+
+    public function updatedTimeFrame(): void
+    {
+        $this->calculateSum();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function calculateSum(): void
+    {
+        $query = Order::query();
+        $timeFrame = TimeFrameEnum::from($this->timeFrame);
+        $parameters = $timeFrame->dateQueryParameters();
+
+        if ($parameters && count($parameters) > 0) {
+            if ($parameters[1] === 'between') {
+                $query->whereBetween($parameters[0], $parameters[2]);
+            } else {
+                $query->where(...$parameters);
+            }
+        }
+
+        $sum = $query->sum('margin');
+        $this->sum = round($sum, 2);
+    }
+
+    public function render(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('flux::livewire.widgets.total-profit',
+            [
+                'currency' => Currency::query()->where('is_default', true)->first()->toArray(),
+                'timeFrames' => TimeFrameEnum::cases(),
+                'selectedTimeFrame' => $this->timeFrame,
+            ]
+        );
+    }
+
+    public static function getLabel(): string
+    {
+        return __('Total Profit');
+    }
+}
