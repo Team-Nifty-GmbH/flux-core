2f660269f60e77d40135a610ec9c39e1e1b857c0
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-08-03 12:05:02 +0200

    use the calculated filename in AttachInvoice.php
    obtain the user label in CommentCreatedNotification.php
    
    remove the log in DeleteMedia.php action (gets logged via activity)
    
    add actions section to order-detail.blade.php

--- src/Actions/Media/DeleteMedia.php
+++ src/Actions/Media/DeleteMedia.php
@@ -2,11 +2,8 @@
 
 namespace FluxErp\Actions\Media;
 
-use Carbon\Carbon;
 use FluxErp\Actions\FluxAction;
 use FluxErp\Models\Media;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class DeleteMedia extends FluxAction
 {
@@ -28,12 +25,6 @@ class DeleteMedia extends FluxAction
         $mediaItem = Media::query()
             ->whereKey($this->data['id'])
             ->first();
-
-        $attributes = $mediaItem->getAttributes();
-        $attributes['deleted_at'] = Carbon::now()->toDateTimeString();
-        $attributes['deleted_by'] = Auth::id();
-        $message = 'File: \'' . $mediaItem->file_name . '\' deleted by user: \'' . Auth::id() . '\'';
-        Log::notice($message, array_merge(['uuid' => $mediaItem->uuid], $attributes));
 
         return $mediaItem->delete();
     }
