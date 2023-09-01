2735ce3b75cadbf9d70b8379dbfc23fba0ab2ba2
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-09-01 14:21:58 +0200

    add sections to order-detail.blade.php

--- resources/views/livewire/portal/order-detail.blade.php
+++ resources/views/livewire/portal/order-detail.blade.php
@@ -19,38 +19,46 @@
      x-on:data-table-record-selected="selected = Alpine.$data(document.getElementById('order-position-table').querySelector('[tall-datatable]')).selected"
 >
     <x-modal.card id="detail-modal" wire:model.defer="detailModal">
-        <div class="grid grid-cols-3 gap-5">
-            <div class="col-span-1">
-                <div class="bg-portal-light w-full rounded-md">
-                    <div x-html="detail.image">
-                    </div>
-                    <div x-show="! detail.image"
-                         class="flex min-h-[12rem] w-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
-                        <svg class="h-12 w-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-                             fill="currentColor" viewBox="0 0 640 512">
-                            <path
-                                d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/>
-                        </svg>
-                    </div>
+        @section('product-modal.content')
+            <div class="grid grid-cols-3 gap-5">
+                <div class="col-span-1">
+                    @section('product-modal.content.image')
+                        <div class="bg-portal-light w-full rounded-md">
+                            <div x-html="detail.image">
+                            </div>
+                            <div x-show="! detail.image"
+                                 class="flex min-h-[12rem] w-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
+                                <svg class="h-12 w-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                                     fill="currentColor" viewBox="0 0 640 512">
+                                    <path
+                                        d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    @show
+                </div>
+                <div class="col-span-2">
+                    @section('product-modal.content.detail')
+                        <div class="text-lg" x-text="detail.product?.product_number"></div>
+                        <div class="text-lg" x-text="detail.name"></div>
+                        <div class="pt-5 text-sm" x-show="detail.serial_number?.length">
+                            <span>{{ __('Serial numbers:') }}</span>
+                            <ul>
+                                <template x-for="serialNumber in detail.serial_number">
+                                    <li x-text="serialNumber.serial_number"></li>
+                                </template>
+                            </ul>
+                        </div>
+                        <div class="pt-5" x-html="detail.product?.description"></div>
+                    @show
                 </div>
             </div>
-            <div class="col-span-2">
-                <div class="text-lg" x-text="detail.product?.product_number"></div>
-                <div class="text-lg" x-text="detail.name"></div>
-                <div class="pt-5 text-sm" x-show="detail.serial_number?.length">
-                    <span>{{ __('Serial numbers:') }}</span>
-                    <ul>
-                        <template x-for="serialNumber in detail.serial_number">
-                            <li x-text="serialNumber.serial_number"></li>
-                        </template>
-                    </ul>
-                </div>
-                <div class="pt-5" x-html="detail.product?.description"></div>
+            <div id="folder-tree" class="pt-3">
+                @section('product-modal.content.media')
+                    <livewire:folder-tree :model-type="\FluxErp\Models\Product::class" />
+                @show
             </div>
-        </div>
-        <div id="folder-tree" class="pt-3">
-            <livewire:folder-tree :model-type="\FluxErp\Models\Product::class" />
-        </div>
+        @show
     </x-modal.card>
     <div id="new-ticket-modal">
         <x-modal.card :title="__('New Ticket')">
