3ab63358e38b910f7c4e677df3b860d582d510b3
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-09-06 14:45:06 +0200

    add missing migration

---
+++ database/migrations/2023_08_29_153011_create_product_cross_selling_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_cross_selling_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_cross_selling_id')
+                ->constrained('product_cross_sellings')
+                ->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+
+            $table->unique(['product_cross_selling_id', 'product_id'], 'product_cross_selling_product_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_cross_selling_product');
+    }
+};
