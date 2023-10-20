df9c3e35a3d9ef959ed5f5a4f313046f5a581bc6
Steffen Franz <der.wichtel@yahoo.de>
2023-10-20 18:47:00 +0200

    merge tests into one workflow fix login browser test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,16 +2,39 @@
 
 namespace FluxErp\Tests\Browser;
 
+use FluxErp\Models\Language;
+use FluxErp\Models\User;
 use FluxErp\Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Laravel\Dusk\Browser;
 
 class LoginTest extends DuskTestCase
 {
+    use DatabaseTransactions;
+
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $language = Language::query()->where('language_code', config('app.locale'))->first();
+        if (! $language) {
+            $language = Language::factory()->create(['language_code' => config('app.locale')]);
+        }
+
+        $this->user = User::factory()->create([
+            'language_id' => $language->id,
+        ]);
+    }
+
     public function test_login(): void
     {
-        $this->browse(function (Browser $browser) {
+        $user = $this->user;
+
+        $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
-                ->type('email', 'test@test.de')
+                ->type('email', $user->email)
                 ->type('password', 'password')
                 ->clickAndWaitForReload('@login-button')
                 ->assertRouteIs('dashboard');
