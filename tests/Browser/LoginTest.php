73b7dbf8238e40e820e6a930534a149ef4f7dd3d
Steffen Franz <der.wichtel@yahoo.de>
2023-10-20 19:35:39 +0200

    fix failed test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,20 +5,33 @@ namespace FluxErp\Tests\Browser;
 use FluxErp\Models\Language;
 use FluxErp\Models\User;
 use FluxErp\Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Laravel\Dusk\Browser;
 
 class LoginTest extends DuskTestCase
 {
-    public function test_login(): void
+    use DatabaseMigrations, DatabaseTransactions;
+
+    private User $user;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $language = Language::query()->where('language_code', config('app.locale'))->first();
         if (! $language) {
             $language = Language::factory()->create(['language_code' => config('app.locale')]);
         }
 
-        $user = User::factory()->create([
+        $this->user = User::factory()->create([
             'language_id' => $language->id,
         ]);
+    }
+
+    public function test_login(): void
+    {
+        $user = $this->user;
 
         $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
