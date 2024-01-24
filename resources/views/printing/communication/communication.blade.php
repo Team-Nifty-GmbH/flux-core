da659f4a771c571a4faa28332ea5c9267a709a06
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-01-24 13:50:53 +0100

    wip

--- resources/views/printing/communication/communication.blade.php
+++ resources/views/printing/communication/communication.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.print>
-    <x-print.first-page-header />
+    <x-print.first-page-header>
+        {!! nl2br(implode('<br>', $model->to)) !!}
+    </x-print.first-page-header>
     <main>
         <div>
             {!! $model->html_body ?? $model->text_body !!}
