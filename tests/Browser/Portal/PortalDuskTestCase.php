ecedc69ea04c8d6991ff41fd569f67da8a8aed1e
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-02-01 16:48:47 +0100

    fix tests

--- tests/Browser/Portal/PortalDuskTestCase.php
+++ tests/Browser/Portal/PortalDuskTestCase.php
@@ -32,7 +32,13 @@ class PortalDuskTestCase extends DuskTestCase
     {
         $this->dbClient = Client::factory()->create();
 
+        Language::factory()->create();
+        // ensure a language with language code 'en' exists
+        if (Language::query()->where('language_code', 'en')->doesntExist()) {
+            Language::factory()->create(['language_code' => 'en']);
+        }
         $language = Language::query()->where('language_code', 'en')->first();
+
         $currency = Currency::factory()->create();
 
         $country = Country::factory()->create([
