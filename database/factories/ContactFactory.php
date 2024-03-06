fa0f35ea43b4d042a24295796aa6d332b519710a
Steffen Franz <der.wichtel@yahoo.de>
2024-03-06 12:47:33 +0100

    fix flaky test

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -21,7 +21,7 @@ class ContactFactory extends Factory
             'discount_days' => $paymentTargetDays
                 ? $this->faker->numberBetween(int2: $paymentTargetDays)
                 : null,
-            'discount_percent' => $this->faker->boolean ? $this->faker->randomFloat(nbMaxDecimals: 2, max: 5) : null,
+            'discount_percent' => $this->faker->boolean ? $this->faker->numberBetween(10, 50) / 100 : null,
             'credit_line' => $this->faker->boolean ?
                 $this->faker->randomFloat(nbMaxDecimals: 2, min: 100, max: 10000) : null,
             'has_delivery_lock' => $this->faker->boolean,
