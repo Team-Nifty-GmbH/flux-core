c10feb8b9311f8c358a5b07bf745ca74234780f0
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-03-07 15:17:28 +0100

    PR changes

--- tests/Feature/Api/PurchaseInvoiceTest.php
+++ tests/Feature/Api/PurchaseInvoiceTest.php
@@ -55,6 +55,7 @@ class PurchaseInvoiceTest extends BaseSetup
         $this->clients = Client::factory()->count(2)->create();
 
         $this->paymentTypes = PaymentType::factory()->count(2)->create([
+            'is_active' => true,
             'client_id' => $this->dbClient->id,
             'is_purchase' => true,
         ]);
