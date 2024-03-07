b9eed7d7792b130b7048f682c75eb02f2807cda2
Steffen Franz <der.wichtel@yahoo.de>
2024-03-07 17:27:46 +0100

    compress InitPermissionsTest.php to shorten tests runtime

--- tests/Feature/Console/InitPermissionsTest.php
+++ tests/Feature/Console/InitPermissionsTest.php
@@ -45,34 +45,7 @@ class InitPermissionsTest extends BaseSetup
             }
         }
 
-        $this->callInitPermissions();
-
-        $this->assertDatabaseCount('roles', count(config('auth.guards')));
-
-        $this->assertGreaterThan(
-            0,
-            Permission::query()
-                ->whereNot('name', 'like', 'widget.%')
-                ->whereNot('name', 'like', 'action.%')
-                ->whereNot('name', 'like', 'tab.%')
-                ->count()
-        );
-
-        // Assert all action permissions created
-        $this->assertEquals($actionsWithPermission, Permission::query()->where('name', 'like', 'action.%')->count());
-
-        // Assert all widget permissions created
-        $this->assertEquals(count(Widget::all()), Permission::query()->where('name', 'like', 'widget.%')->count());
-
-        // Assert all tab permissions created
-        $this->assertEquals(
-            count(array_unique($componentTabs)),
-            Permission::query()->where('name', 'like', 'tab.%')->count()
-        );
-    }
-
-    public function test_init_permissions_with_custom_widget()
-    {
+        // Add Custom Widget
         Livewire::component('custom-widget-that-never-exists', new class extends Component
         {
             use Widgetable;
@@ -92,41 +65,55 @@ class InitPermissionsTest extends BaseSetup
 
         Widget::register('custom-widget-that-never-exists', 'custom-widget-that-never-exists');
 
-        $this->callInitPermissions();
-
-        $this->assertDatabaseHas('permissions', ['name' => 'widget.custom-widget-that-never-exists']);
-    }
-
-    public function test_init_permissions_with_custom_tab()
-    {
+        // Add Custom Tab
         Event::listen('tabs.rendering: ' . Product::class, function (Component $component) {
             $component->mergeTabsToRender([
                 TabButton::make('custom-tab-that-never-exists', label: 'Custom Tab'),
             ]);
         });
 
-        $this->callInitPermissions();
-
-        $this->assertDatabaseHas('permissions', ['name' => 'tab.custom-tab-that-never-exists']);
-    }
-
-    public function test_init_permissions_unused_permissions()
-    {
+        // Add unused permission
         Permission::create(['name' => 'unused.permission']);
         $this->assertDatabaseHas('permissions', ['name' => 'unused.permission']);
 
-        $this->callInitPermissions();
-
-        $this->assertDatabaseMissing('permissions', ['name' => 'unused.permission']);
-    }
-
-    private function callInitPermissions(): void
-    {
+        // Execute artisan command
         $this->artisan('init:permissions')
             ->assertExitCode(0)
             ->expectsOutput('Registering action permissions…')
             ->expectsOutput('Registering route permissions…')
             ->expectsOutput('Registering widget permissions…')
             ->expectsOutput('Registering tab permissions…');
+
+        $this->assertDatabaseCount('roles', count(config('auth.guards')));
+
+        $this->assertGreaterThan(
+            0,
+            Permission::query()
+                ->whereNot('name', 'like', 'widget.%')
+                ->whereNot('name', 'like', 'action.%')
+                ->whereNot('name', 'like', 'tab.%')
+                ->count()
+        );
+
+        // Assert all action permissions created
+        $this->assertEquals($actionsWithPermission, Permission::query()->where('name', 'like', 'action.%')->count());
+
+        // Assert all widget permissions created
+        $this->assertEquals(count(Widget::all()), Permission::query()->where('name', 'like', 'widget.%')->count());
+
+        // Assert all tab permissions created (plus one for custom tab)
+        $this->assertEquals(
+            count(array_unique($componentTabs)) + 1,
+            Permission::query()->where('name', 'like', 'tab.%')->count()
+        );
+
+        // Assert custom widget permission created
+        $this->assertDatabaseHas('permissions', ['name' => 'widget.custom-widget-that-never-exists']);
+
+        // Assert custom tab permission created
+        $this->assertDatabaseHas('permissions', ['name' => 'tab.custom-tab-that-never-exists']);
+
+        // Assert unused permission removed
+        $this->assertDatabaseMissing('permissions', ['name' => 'unused.permission']);
     }
 }
