9d5d67445ec811d3ba36756b290dc91812bdd20b
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-04-03 09:30:42 +0200

    add vite config keys for reverb

--- resources/views/components/layouts/head/head.blade.php
+++ resources/views/components/layouts/head/head.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta name="apple-mobile-web-app-capable" content="yes">
-<link rel="manifest" href="{{ route('manifest') }}n">
+<link rel="manifest" href="{{ route('manifest') }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="ws-key" content="{{ config('flux.vite.reverb_app_key') }}">
 <meta name="ws-broadcaster" content="{{ config('broadcasting.default', 'reverb') }}">
