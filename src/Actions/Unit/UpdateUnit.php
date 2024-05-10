8ddce6f5912c5d1bba06e472d4d5c93d7ea76577
sajberkurajber90 <tiborice@gmail.com>
2024-05-10 08:24:42 +0200

    create layout and add missing update ruleset and action

---
+++ src/Actions/Unit/UpdateUnit.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FluxErp\Actions\Unit;
+
+use FluxErp\Actions\FluxAction;
+use FluxErp\Models\Unit;
+use FluxErp\Rulesets\Unit\UpdateUnitRuleset;
+use Illuminate\Database\Eloquent\Model;
+
+class UpdateUnit extends FluxAction
+{
+    protected function boot(array $data): void
+    {
+        parent::boot($data);
+        $this->rules = resolve_static(UpdateUnitRuleset::class, 'getRules');
+    }
+
+    public static function models(): array
+    {
+        return [Unit::class];
+    }
+
+    public function performAction(): Model
+    {
+        $unit = app(Unit::class)->query()
+            ->whereKey($this->data['id'])
+            ->first();
+
+        $unit->fill($this->data);
+        $unit->save();
+
+        return $unit->withoutRelations()->fresh();
+    }
+}
