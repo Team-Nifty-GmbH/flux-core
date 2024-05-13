d04cebf65e9277ddaddbb62e4ec4bc79840e641b
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-05-13 15:51:21 +0200

    add asset-install command to build assets in project

---
+++ database/migrations/2024_05_13_110905_add_foreign_keys_to_orders_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreign('currency_id')->references('id')->on('currencies');
+            $table->foreign('price_list_id')->references('id')->on('price_lists');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['currency_id']);
+            $table->dropForeign(['price_list_id']);
+        });
+    }
+};
---
+++ src/Console/Commands/InstallAssets.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace FluxErp\Console\Commands;
+
+use Illuminate\Console\Command;
+use RuntimeException;
+use Symfony\Component\Process\Process;
+
+class InstallAssets extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'flux:install-assets {--force : Overwrite existing files}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->callSilent('storage:link');
+
+
+        // require npm packages
+        $this->info('Installing npm packages...');
+
+
+        // Tailwind Configuration...
+        if (! file_exists(base_path('tailwind.config.js')) || $this->option('force')) {
+            $this->info('Publishing Tailwind Configuration...');
+            copy(__DIR__.'/../../../stubs/tailwind/tailwind.config.js', base_path('tailwind.config.js'));
+        }
+
+        if (! file_exists(base_path('postcss.config.js')) || $this->option('force')) {
+            $this->info('Publishing PostCSS Configuration...');
+            copy(__DIR__.'/../../../stubs/tailwind/postcss.config.js', base_path('postcss.config.js'));
+        }
+
+        if (! file_exists(base_path('vite.config.js')) || $this->option('force')) {
+            $this->info('Publishing Vite Configuration...');
+            copy(__DIR__.'/../../../stubs/tailwind/vite.config.js', base_path('vite.config.js'));
+        }
+
+        $this->updateNodePackages(function ($packages) {
+            return [
+                    '@fontsource/inter' => "^5.0.18",
+                    '@fullcalendar/core' => '^6.1.11',
+                    '@fullcalendar/daygrid' => '^6.1.10',
+                    '@fullcalendar/interaction' => '^6.1.10',
+                    '@fullcalendar/list' => '^6.1.11',
+                    '@fullcalendar/timegrid' => '^6.1.10',
+                    '@tailwindcss/forms' => '^0.5.7',
+                    '@tailwindcss/typography' => '^0.5.10',
+                    'autoprefixer' => '^10.4.16',
+                    'postcss' => '^8.4.32',
+                    'tailwindcss' => '^3.4.0',
+                    'tributejs' => '^5.1.3',
+                ] + $packages;
+        });
+
+        if (file_exists(resource_path('views/welcome.blade.php'))) {
+            unlink(resource_path('views/welcome.blade.php'));
+        }
+
+        $this->runCommands(['npm install', 'npm run build']);
+    }
+
+    protected static function updateNodePackages(callable $callback, $dev = true): void
+    {
+        if (! file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages[$configurationKey] = $callback(
+            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
+            $configurationKey
+        );
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
+        );
+    }
+
+    protected function runCommands($commands): void
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
+    }
+}
