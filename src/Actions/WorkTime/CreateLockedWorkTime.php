1844487caa2628da82bf7d831245bb193e2a3290
Steffen Franz <der.wichtel@yahoo.de>
2024-05-22 19:56:40 +0200

    fix create locked work time pr

--- src/Actions/WorkTime/CreateLockedWorkTime.php
+++ src/Actions/WorkTime/CreateLockedWorkTime.php
@@ -5,6 +5,7 @@ namespace FluxErp\Actions\WorkTime;
 use FluxErp\Models\WorkTime;
 use FluxErp\Rulesets\WorkTime\CreateLockedWorkTimeRuleset;
 use Illuminate\Support\Carbon;
+use Illuminate\Validation\ValidationException;
 
 class CreateLockedWorkTime extends CreateWorkTime
 {
@@ -18,7 +19,15 @@ class CreateLockedWorkTime extends CreateWorkTime
     {
         $workTime = app(WorkTime::class, ['attributes' => $this->data]);
 
+        $workTime->paused_time_ms ??= 0;
         $workTime->is_billable ??= $workTime->workTimeType?->is_billable ?? false;
+
+        if ($workTime->ended_at) {
+            $workTime->total_time_ms = $workTime->ended_at->diffInMilliseconds($workTime->started_at)
+                - $workTime->paused_time_ms;
+        } else {
+            $workTime->is_locked = false;
+        }
 
         $workTime->save();
 
@@ -27,21 +36,33 @@ class CreateLockedWorkTime extends CreateWorkTime
 
     protected function prepareForValidation(): void
     {
-        $this->data['user_id'] ??= auth()->user()->id;
-
-        if (! is_null($this->data['user_id']) && ! is_null($this->data['started_at'])) {
+        if (data_get($this->data, 'user_id') && data_get($this->data, 'started_at')) {
             // add parent_id in case daily work time exists
-            $this->data['parent_id'] = app(WorkTime::class)::query()->where('user_id', $this->data['user_id'])
+            $this->data['parent_id'] = app(WorkTime::class)::query()
+                ->where('user_id', $this->data['user_id'])
+                ->where('started_at', '<=', Carbon::parse($this->data['started_at'])->toDateTimeString())
+                ->where(function ($query) {
+                    $query->where('ended_at', '>', Carbon::parse($this->data['started_at'])->toDateTimeString())
+                        ->orWhereNull('ended_at');
+                })
                 ->where('is_daily_work_time', true)
-                ->whereDate('started_at', Carbon::parse($this->data['started_at'])->toDateString())
-                ->first()->id ?? null;
+                ->value('id');
         }
+    }
 
-        $this->data['paused_time_ms'] ??= 0;
+    protected function validateData(): void
+    {
+        parent::validateData();
 
-        $this->data['total_time_ms'] = Carbon::parse(data_get($this->data, 'ended_at', 0))
-            ->diffInMilliseconds(Carbon::parse(data_get($this->data, 'started_at', 0)))
-            - data_get($this->data, 'paused_time_ms');
+        if ($endedAt = data_get($this->data, 'ended_at')) {
+            $totalTimeMs = Carbon::parse($endedAt)->diffInMilliseconds(Carbon::parse($this->data['started_at']))
+                - data_get($this->data, 'paused_time_ms', 0);
 
+            if ($totalTimeMs < 0) {
+                throw ValidationException::withMessages([
+                    'paused_time_ms' => [__('Pause can not be longer than time between started_at and ended_at.')],
+                ])->errorBag('createLockedWorkTime');
+            }
+        }
     }
 }
--- src/Rulesets/WorkTime/CreateLockedWorkTimeRuleset.php
+++ src/Rulesets/WorkTime/CreateLockedWorkTimeRuleset.php
@@ -12,15 +12,15 @@ class CreateLockedWorkTimeRuleset extends CreateWorkTimeRuleset
 
         return array_merge(
             parent::rules(),
-            ['started_at' => 'required_with:ended_at|date_format:Y-m-d H:i:s|before:now',
-                'ended_at' => 'date_format:Y-m-d H:i:s|after:started_at',
+            [
                 'parent_id' => [
                     'nullable',
                     'integer',
                     new ModelExists(WorkTime::class),
                 ],
+                'started_at' => 'required|date',
+                'ended_at' => 'nullable|date|after:started_at',
                 'paused_time_ms' => 'integer|nullable|min:0',
-                'total_time_ms' => 'integer|min:0',
             ]
         );
     }
