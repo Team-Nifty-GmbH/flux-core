e648c3c8c76b253e5efefe66b69361b8e80562b9
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-05-31 18:16:51 +0200

    fix tests

--- src/Http/Middleware/Permissions.php
+++ src/Http/Middleware/Permissions.php
@@ -3,6 +3,7 @@
 namespace FluxErp\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -13,7 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class Permissions
 {
-    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (
             ! Auth::user()
