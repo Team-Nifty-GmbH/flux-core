a22985ddcbad0a4e76133942ef937f2fe249a54d
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-07-26 11:19:18 +0200

    fix order confirmation

--- src/Livewire/Portal/Shop/Checkout.php
+++ src/Livewire/Portal/Shop/Checkout.php
@@ -112,7 +112,7 @@ class Checkout extends Cart
         event(new PortalOrderCreated($order));
 
         if (auth('address')->check()) {
-            Mail::to(auth('address')->user())->queue(new OrderConfirmation($order));
+            Mail::to(auth('address')->user())->queue(new OrderConfirmation($order->refresh()));
         }
 
         $this->redirect(route('portal.checkout-finish'), true);
