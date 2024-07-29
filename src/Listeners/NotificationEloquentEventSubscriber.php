e80f58862340b724fcc5e6f9fc20308e06d74f8a
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-07-29 19:16:59 +0200

    fix notification subscriber

--- src/Listeners/NotificationEloquentEventSubscriber.php
+++ src/Listeners/NotificationEloquentEventSubscriber.php
@@ -20,7 +20,7 @@ class NotificationEloquentEventSubscriber
     public function sendNotification($event, $model): void
     {
         $eventType = explode('.', explode(':', $event)[0])[1];
-        $notification = config('notifications.model_notifications.' . $model[0]->getMorphClass() . '.' . $eventType);
+        $notification = config('notifications.model_notifications.' . get_class($model[0]) . '.' . $eventType);
 
         if (! $notification) {
             return;
