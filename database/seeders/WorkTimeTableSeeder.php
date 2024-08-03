0c3ad9b8b5953ca9f5d4c19746cee8bddd438f7a
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-08-03 12:03:26 +0200

    pint

--- database/seeders/WorkTimeTableSeeder.php
+++ database/seeders/WorkTimeTableSeeder.php
@@ -27,13 +27,13 @@ class WorkTimeTableSeeder extends Seeder
                 $startedAt = fake()->dateTimeBetween($dailyWorkTime->started_at, $dailyWorkTime->ended_at);
                 WorkTime::factory(80)->create([
                     'user_id' => $user->id,
-                    'contact_id' => fake()->boolean(70)
+                    'contact_id' => fn() => fake()->boolean(70)
                            ? $contacts->random()->id
                            : null,
                     'started_at' => $startedAt,
-                    'ended_at' => fake()->dateTimeBetween($startedAt, $dailyWorkTime->ended_at),
+                    'ended_at' => fn() => fake()->dateTimeBetween($startedAt, $dailyWorkTime->ended_at),
                     'parent_id' => $dailyWorkTime->id,
-                    'work_time_type_id' => $workTimeTypes->random()->id,
+                    'work_time_type_id' => fn() => $workTimeTypes->random()->id,
                     'is_daily_work_time' => false,
                 ]);
             }
