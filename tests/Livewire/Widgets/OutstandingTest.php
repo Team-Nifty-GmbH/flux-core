f2784a683e029ed22135fb4b0961eb22d58ee72f
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-09-03 10:48:51 +0200

    fix tests

--- tests/Livewire/Widgets/OutstandingTest.php
+++ tests/Livewire/Widgets/OutstandingTest.php
@@ -3,12 +3,20 @@
 namespace Tests\Feature\Livewire\Widgets;
 
 use FluxErp\Livewire\Widgets\Outstanding;
+use FluxErp\Models\Currency;
 use FluxErp\Tests\TestCase;
 use Livewire\Livewire;
 
 class OutstandingTest extends TestCase
 {
     protected string $livewireComponent = Outstanding::class;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Currency::factory()->create(['is_default' => true]);
+    }
 
     public function test_renders_successfully()
     {
