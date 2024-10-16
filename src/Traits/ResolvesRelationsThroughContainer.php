8c26b5c821e066bb665b6f15202bb15f996f69ff
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-10-16 14:07:25 +0200

    PR changes

--- src/Traits/ResolvesRelationsThroughContainer.php
+++ src/Traits/ResolvesRelationsThroughContainer.php
@@ -2,14 +2,16 @@
 
 namespace FluxErp\Traits;
 
+use Illuminate\Database\Eloquent\Model;
+
 trait ResolvesRelationsThroughContainer
 {
-    protected function newRelatedInstance($class)
+    protected function newRelatedInstance($class): Model
     {
         return parent::newRelatedInstance(resolve_static($class, 'class'));
     }
 
-    protected function newRelatedThroughInstance($class)
+    protected function newRelatedThroughInstance($class): Model
     {
         return parent::newRelatedThroughInstance(resolve_static($class, 'class'));
     }
