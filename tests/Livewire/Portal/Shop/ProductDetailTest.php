0b5e5cc7d3f9d34f2fdd6816dd372cc5498e6dde
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-11-02 08:50:52 +0100

    fix bundle in portal

--- tests/Livewire/Portal/Shop/ProductDetailTest.php
+++ tests/Livewire/Portal/Shop/ProductDetailTest.php
@@ -4,6 +4,7 @@ namespace FluxErp\Tests\Livewire\Portal\Shop;
 
 use FluxErp\Livewire\Portal\Shop\ProductDetail;
 use FluxErp\Models\Currency;
+use FluxErp\Models\Product;
 use FluxErp\Tests\TestCase;
 use Livewire\Livewire;
 
@@ -20,7 +21,54 @@ class ProductDetailTest extends TestCase
 
     public function test_renders_successfully()
     {
-        Livewire::test(ProductDetail::class)
-            ->assertStatus(200);
+        $product = Product::factory()->create();
+
+        Livewire::test(ProductDetail::class, ['product' => $product])
+            ->assertStatus(200)
+            ->assertSet('productForm.id', $product->id)
+            ->assertSet('productForm.parent_id', $product->parent_id)
+            ->assertSet('productForm.product_number', $product->product_number)
+            ->assertSet('productForm.name', $product->name)
+            ->assertSet('productForm.description', $product->description)
+            ->assertSee([
+                $product->product_number,
+                $product->name,
+                $product->description,
+            ]);
+    }
+
+    public function test_shows_bundle_products()
+    {
+        $product = Product::factory()
+            ->has(
+                Product::factory()
+                    ->has(Product::factory()->count(2), 'bundleProducts')
+                    ->state([
+                        'is_nos' => true,
+                        'is_active_export_to_web_shop' => true,
+                        'is_active' => true,
+                    ])
+                    ->count(3),
+                'bundleProducts'
+            )
+            ->state([
+                'is_nos' => true,
+                'is_active_export_to_web_shop' => true,
+                'is_active' => true,
+            ])
+            ->create();
+
+        $component = Livewire::test(ProductDetail::class, ['product' => $product])
+            ->assertStatus(200)
+            ->assertSet('productForm.id', $product->id)
+            ->assertSet('productForm.parent_id', $product->parent_id)
+            ->assertSet('productForm.product_number', $product->product_number)
+            ->assertCount('productForm.bundle_products', 9)
+            ->assertSee([
+                $product->product_number,
+                $product->name,
+                $product->description,
+            ])
+            ->assertSee($product->bundleProducts()->pluck('name')->toArray());
     }
 }
