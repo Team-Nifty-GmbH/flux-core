6ac2b240532a078dbca5fe548dd6d0ef125e5931
Steffen Franz <der.wichtel@yahoo.de>
2024-11-06 14:46:39 +0100

    fix failed test

--- tests/Livewire/Address/TasksTest.php
+++ tests/Livewire/Address/TasksTest.php
@@ -3,6 +3,8 @@
 namespace FluxErp\Tests\Livewire\Address;
 
 use FluxErp\Livewire\Address\Tasks;
+use FluxErp\Models\Address;
+use FluxErp\Models\Contact;
 use FluxErp\Tests\Livewire\BaseSetup;
 use Livewire\Livewire;
 
@@ -10,7 +12,18 @@ class TasksTest extends BaseSetup
 {
     public function test_renders_successfully()
     {
-        Livewire::test(Tasks::class, ['modelId' => $this->address->id])
+        $contact = Contact::factory()->create([
+            'client_id' => $this->dbClient->id,
+        ]);
+        $address = Address::factory()->create([
+            'contact_id' => $contact->id,
+            'client_id' => $this->dbClient->id,
+            'language_id' => $this->user->language_id,
+            'can_login' => false,
+            'is_active' => true,
+        ]);
+
+        Livewire::test(Tasks::class, ['modelId' => $address->id])
             ->assertStatus(200);
     }
 }
