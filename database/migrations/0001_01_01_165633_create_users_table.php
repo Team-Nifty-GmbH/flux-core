ee720266c0abfce28afda9d819785e9baad25b2e
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-11-21 11:06:41 +0100

    squash migrations

--- database/migrations/0001_01_01_165633_create_users_table.php
+++ database/migrations/0001_01_01_165633_create_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->char('uuid', 36)->unique();
             $table->unsignedBigInteger('contact_id')->nullable()->index('users_contact_id_foreign');
+            $table->unsignedBigInteger('language_id')->index('users_language_id_foreign');
+            $table->unsignedBigInteger('parent_id')->nullable()->index('users_parent_id_foreign');
+            $table->unsignedBigInteger('currency_id')->nullable()->index('users_currency_id_foreign');
             $table->string('email')->unique();
             $table->string('firstname');
             $table->string('lastname');
@@ -35,10 +39,6 @@ return new class extends Migration
             $table->text('two_factor_recovery_codes')->nullable();
             $table->timestamp('two_factor_confirmed_at')->nullable();
             $table->rememberToken();
-            $table->char('uuid', 36)->unique();
-            $table->unsignedBigInteger('language_id')->index('users_language_id_foreign');
-            $table->unsignedBigInteger('parent_id')->nullable()->index('users_parent_id_foreign');
-            $table->unsignedBigInteger('currency_id')->nullable()->index('users_currency_id_foreign');
             $table->timestamp('created_at')->nullable()->comment('A timestamp reflecting the time of record-creation.');
             $table->string('created_by')->nullable();
             $table->timestamp('updated_at')->nullable()->comment('A timestamp reflecting the time of the last change for this record.');
