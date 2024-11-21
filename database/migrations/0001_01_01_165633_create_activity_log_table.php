683683c5790a283c00575aa3c8bde4173250bfed
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-11-21 11:27:04 +0100

    squash migrations

---
+++ database/migrations/0001_01_01_165633_create_activity_log_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('causer_type')->nullable();
+            $table->unsignedBigInteger('causer_id')->nullable();
+            $table->string('subject_type')->nullable();
+            $table->unsignedBigInteger('subject_id')->nullable();
+            $table->string('event')->nullable();
+            $table->string('log_name')->nullable();
+            $table->text('description');
+            $table->json('properties')->nullable();
+            $table->char('batch_uuid', 36)->nullable();
+            $table->timestamps();
+
+            $table->index(['log_name', 'event']);
+            $table->index(['causer_type', 'causer_id'], 'causer');
+            $table->index(['subject_type', 'subject_id'], 'subject');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_log');
+    }
+};
