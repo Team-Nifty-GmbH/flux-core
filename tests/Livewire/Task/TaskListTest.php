b4c251f7a2442d8f5ea3e0d0e99c64cec8d71308
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-11-21 16:51:07 +0100

    add responsible user default to tasks

--- tests/Livewire/Task/TaskListTest.php
+++ tests/Livewire/Task/TaskListTest.php
@@ -5,6 +5,7 @@ namespace FluxErp\Tests\Livewire\Task;
 use FluxErp\Livewire\Task\TaskList;
 use FluxErp\Tests\Livewire\BaseSetup;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
 use Livewire\Livewire;
 
 class TaskListTest extends BaseSetup
@@ -15,5 +16,31 @@ class TaskListTest extends BaseSetup
     {
         Livewire::test(TaskList::class)
             ->assertStatus(200);
+    }
+
+    public function test_create_new_task()
+    {
+        Livewire::actingAs($this->user)
+            ->test(TaskList::class)
+            ->call('resetForm')
+            ->assertSet('task.responsible_user_id', $this->user->id)
+            ->set('task.name', $taskName = Str::uuid())
+            ->set('task.description', $taskDescription = Str::uuid())
+            ->set('task.due_date', $taskDueDate = now()->addDays(4)->format('Y-m-d'))
+            ->set('task.start_date', $taskStartDate = now()->addDays(3)->format('Y-m-d'))
+            ->set('task.priority', 1)
+            ->call('save')
+            ->assertHasNoErrors()
+            ->assertStatus(200)
+            ->assertReturned(true);
+
+        $this->assertDatabaseHas('tasks', [
+            'responsible_user_id' => $this->user->id,
+            'name' => $taskName,
+            'description' => $taskDescription,
+            'due_date' => $taskDueDate,
+            'start_date' => $taskStartDate,
+            'priority' => 1,
+        ]);
     }
 }
