e59cee6d646f34780b4cffe3e1bf7b9df9a7b3a1
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-11-26 09:22:40 +0100

    wip

--- resources/views/livewire/order/order-positions-move.blade.php
+++ resources/views/livewire/order/order-positions-move.blade.php
@@ -1,5 +1,6 @@
-<x-flux::sortable
+<x-flux::features.sortable.sortable
     :data="$orderPositions"
     x-sort="$wire.reOrder($item, $position)"
+    x-sort:group="order-positions"
     x-sort:config="{fallbackOnBody: true, swapThreshold: 0.65}"
 />
