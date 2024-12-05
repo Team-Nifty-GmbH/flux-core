f09ad94e37c6a35b21ca4c94579cb7d16d135b1e
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-12-05 08:31:29 +0100

    fix password reset on address

--- tests/Livewire/Contact/AddressesTest.php
+++ tests/Livewire/Contact/AddressesTest.php
@@ -8,6 +8,7 @@ use FluxErp\Livewire\Forms\ContactForm;
 use FluxErp\Models\Address;
 use FluxErp\Models\Contact;
 use FluxErp\Tests\Livewire\BaseSetup;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Livewire\Livewire;
 
@@ -74,5 +75,34 @@ class AddressesTest extends BaseSetup
             ->assertSet('edit', false);
 
         $this->assertDatabaseHas('addresses', ['id' => $this->addressForm->id, 'street' => $street]);
+    }
+
+    public function test_can_update_password()
+    {
+        Address::query()
+            ->whereKey($this->addressForm->id)
+            ->update([
+                'can_login' => 1,
+                'password' => Hash::make('!password123'),
+            ]);
+
+        Livewire::actingAs($this->user)
+            ->test(Addresses::class, ['contact' => $this->contactForm, 'address' => $this->addressForm])
+            ->assertSet('address.password', null)
+            ->set('address.password', $password = Hash::make(Str::random()))
+            ->set('edit', true)
+            ->call('save')
+            ->assertStatus(200)
+            ->assertHasNoErrors()
+            ->assertSet('address.password', $password)
+            ->assertSet('edit', false);
+
+        $this->assertDatabaseHas(
+            'addresses',
+            [
+                'id' => $this->addressForm->id,
+                'password' => $password,
+            ]
+        );
     }
 }
