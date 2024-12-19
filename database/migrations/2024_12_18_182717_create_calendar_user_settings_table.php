4f9c2142a7ece62759bf8b9896744cab797b3237
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-12-19 13:54:23 +0100

    wip

---
+++ database/migrations/2024_12_18_182717_create_calendar_user_settings_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('calendar_user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('authenticatable', 'datatable_authenticatable');
+            $table->string('cache_key')->index();
+            $table->string('component')->index();
+            $table->json('settings');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('calendar_user_settings');
+    }
+};
