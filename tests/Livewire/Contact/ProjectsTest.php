454ba837bc8aa7f6cca37ab356185e5147f15db4
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-12-20 11:06:07 +0100

    fix tests

--- tests/Livewire/Contact/ProjectsTest.php
+++ tests/Livewire/Contact/ProjectsTest.php
@@ -3,14 +3,24 @@
 namespace FluxErp\Tests\Livewire\Contact;
 
 use FluxErp\Livewire\Contact\Projects;
-use FluxErp\Tests\TestCase;
+use FluxErp\Models\Contact;
+use FluxErp\Tests\Livewire\BaseSetup;
 use Livewire\Livewire;
 
-class ProjectsTest extends TestCase
+class ProjectsTest extends BaseSetup
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->contact = Contact::factory()->create([
+            'client_id' => $this->dbClient->id,
+        ]);
+    }
+
     public function test_renders_successfully()
     {
-        Livewire::test(Projects::class)
+        Livewire::test(Projects::class, ['contactId' => $this->contact->id])
             ->assertStatus(200);
     }
 }
