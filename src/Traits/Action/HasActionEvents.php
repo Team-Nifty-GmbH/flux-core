93a82ff32cef6387febf9ab0b8086ea72709fe56
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2024-12-22 17:00:56 +0100

    refactor action broadcasting

---
+++ src/Traits/Action/HasActionEvents.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace FluxErp\Traits\Action;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Events\QueuedClosure;
+
+trait HasActionEvents
+{
+    public static function booting(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('booting', $callback);
+    }
+
+    public static function booted(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('booted', $callback);
+    }
+
+    public static function executing(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('executing', $callback);
+    }
+
+    public static function executed(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('executed', $callback);
+    }
+
+    public static function preparingForValidation(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('preparingForValidation', $callback);
+    }
+
+    public static function validating(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('validating', $callback);
+    }
+
+    public static function validated(QueuedClosure|callable|array|string $callback): void
+    {
+        static::registerActionEvent('validated', $callback);
+    }
+
+    protected static function registerActionEvent($event, $callback): void
+    {
+        if (isset(static::$dispatcher)) {
+            $name = static::class;
+
+            static::$dispatcher->listen('action.' . $event . ': ' . $name, $callback);
+        }
+    }
+
+    protected function fireActionEvent(string $event, bool $halt = true): mixed
+    {
+        if (! isset(static::$dispatcher)) {
+            return null;
+        }
+
+        $function = $halt ? 'until' : 'dispatch';
+
+        return static::$dispatcher->{$function}('action.' . $event . ': ' . static::class, $this);
+    }
+
+    public static function setEventDispatcher(?Dispatcher $dispatcher = null): void
+    {
+        static::$dispatcher = $dispatcher;
+    }
+
+    final public function withEvents(): static
+    {
+        $this->setEventDispatcher(app('events'));
+
+        return $this;
+    }
+
+    final public function withoutEvents(): static
+    {
+        $this->setEventDispatcher();
+
+        return $this;
+    }
+}
