1d5c2b48be856bbab2bd1e196860866d80e739ca
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2025-01-09 14:54:03 +0100

    add new sections to contact

--- resources/views/livewire/contact/contact.blade.php
+++ resources/views/livewire/contact/contact.blade.php
@@ -18,7 +18,7 @@
                                 {{ data_get($contact->main_address, 'name') }}
                             </h1>
                         </div>
-                    @endsection
+                    @show
                 @show
             </div>
             <div class="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
