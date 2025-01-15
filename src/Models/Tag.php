6a8900fc1b1c90f8e7e6849af4dff4a56d840865
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2025-01-15 09:20:54 +0100

    remove translatable from tags

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -4,12 +4,22 @@ namespace FluxErp\Models;
 
 use FluxErp\Traits\ResolvesRelationsThroughContainer;
 use FluxErp\Traits\Scout\Searchable;
+use Illuminate\Support\Str;
 use Spatie\Tags\Tag as BaseTag;
 use TeamNiftyGmbH\DataTable\Contracts\InteractsWithDataTables;
 
 class Tag extends BaseTag implements InteractsWithDataTables
 {
     use ResolvesRelationsThroughContainer, Searchable;
+
+    public array $translatable = [];
+
+    public static function bootHasSlug(): void
+    {
+        static::saving(function (Tag $model) {
+            $model->slug = Str::slug($model->name);
+        });
+    }
 
     public function getLabel(): ?string
     {
