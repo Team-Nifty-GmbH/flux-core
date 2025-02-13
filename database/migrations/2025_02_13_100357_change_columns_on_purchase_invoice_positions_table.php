9cf23936a0773dd9d9ff815daadc5db71e5ecb1b
patrickweh <pw@team-nifty.com>
2025-02-13 11:14:33 +0100

    allow negative purchase invoice positions

---
+++ database/migrations/2025_02_13_100357_change_columns_on_purchase_invoice_positions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('purchase_invoice_positions', function (Blueprint $table) {
+            $table->decimal('amount', 40, 10)->change();
+            $table->decimal('unit_price', 40, 10)->change();
+            $table->decimal('total_price', 40, 10)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        DB::table('purchase_invoice_positions')
+            ->update([
+                'amount' => DB::raw('ABS(amount)'),
+                'unit_price' => DB::raw('ABS(unit_price)'),
+                'total_price' => DB::raw('ABS(total_price)'),
+            ]);
+
+        Schema::table('purchase_invoice_positions', function (Blueprint $table) {
+            $table->decimal('amount', 40, 10)->unsigned()->change();
+            $table->decimal('unit_price', 40, 10)->unsigned()->change();
+            $table->decimal('total_price', 40, 10)->unsigned()->change();
+        });
+    }
+};
