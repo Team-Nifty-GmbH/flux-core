574f6e1d38990e6a1cedb99f5effd6125b66745a
patrickweh <pw@team-nifty.com>
2025-03-04 12:35:35 +0100

    fix labels for apex charts

--- resources/views/support/widgets/charts/circle-chart.blade.php
+++ resources/views/support/widgets/charts/circle-chart.blade.php
@@ -1,24 +1,26 @@
 @extends('flux::support.widgets.charts.chart')
 @section('options')
     @parent
-    <x-native-select
-        x-model="chartType"
-        :options="[
-                [
-                    'value' => 'donut',
-                    'label' => __('Donut'),
-                ],
-                [
-                    'value' => 'polarArea',
-                    'label' => __('Polar Area'),
-                ],
-                [
-                    'value' => 'pie',
-                    'label' => __('Pie'),
-                ],
-            ]"
-        option-value="value"
-        option-label="label"
-    >
-    </x-native-select>
+    <div class="px-6 py-2">
+        <x-native-select
+            x-model="chartType"
+            :options="[
+                    [
+                        'value' => 'donut',
+                        'label' => __('Donut'),
+                    ],
+                    [
+                        'value' => 'polarArea',
+                        'label' => __('Polar Area'),
+                    ],
+                    [
+                        'value' => 'pie',
+                        'label' => __('Pie'),
+                    ],
+                ]"
+            option-value="value"
+            option-label="label"
+        >
+        </x-native-select>
+    </div>
 @endsection
