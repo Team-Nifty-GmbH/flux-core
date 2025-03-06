74fa81e3ac2b6dbaf311c148867b5c3770deac40
patrickweh <pw@team-nifty.com>
2025-03-06 18:06:23 +0100

    PR changes

--- resources/views/support/widgets/charts/circle-chart.blade.php
+++ resources/views/support/widgets/charts/circle-chart.blade.php
@@ -5,19 +5,19 @@
         <x-native-select
             x-model="chartType"
             :options="[
-                    [
-                        'value' => 'donut',
-                        'label' => __('Donut'),
-                    ],
-                    [
-                        'value' => 'polarArea',
-                        'label' => __('Polar Area'),
-                    ],
-                    [
-                        'value' => 'pie',
-                        'label' => __('Pie'),
-                    ],
-                ]"
+                [
+                    'value' => 'donut',
+                    'label' => __('Donut'),
+                ],
+                [
+                    'value' => 'polarArea',
+                    'label' => __('Polar Area'),
+                ],
+                [
+                    'value' => 'pie',
+                    'label' => __('Pie'),
+                ],
+            ]"
             option-value="value"
             option-label="label"
         >
