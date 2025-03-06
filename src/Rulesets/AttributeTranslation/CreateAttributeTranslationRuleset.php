4898dbad6c56616359f2b1994d74a51dcee197eb
patrickweh <pw@team-nifty.com>
2025-03-06 14:21:38 +0100

    add rules

--- src/Rulesets/AttributeTranslation/CreateAttributeTranslationRuleset.php
+++ src/Rulesets/AttributeTranslation/CreateAttributeTranslationRuleset.php
@@ -2,12 +2,42 @@
 
 namespace FluxErp\Rulesets\AttributeTranslation;
 
+use FluxErp\Models\Language;
+use FluxErp\Rules\ModelExists;
+use FluxErp\Rules\MorphClassExists;
+use FluxErp\Rules\MorphExists;
 use FluxErp\Rulesets\FluxRuleset;
+use FluxErp\Traits\HasAttributeTranslations;
 
 class CreateAttributeTranslationRuleset extends FluxRuleset
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'language_id' => [
+                'required',
+                'integer',
+                app(ModelExists::class, ['model' => Language::class]),
+            ],
+            'model_type' => [
+                'required',
+                'string',
+                app(MorphClassExists::class, ['uses' => HasAttributeTranslations::class]),
+            ],
+            'model_id' => [
+                'required',
+                'integer',
+                app(MorphExists::class),
+            ],
+            'attribute' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'value' => [
+                'required',
+                'string',
+            ],
+        ];
     }
 }
--- src/Rulesets/AttributeTranslation/UpdateAttributeTranslationRuleset.php
+++ src/Rulesets/AttributeTranslation/UpdateAttributeTranslationRuleset.php
@@ -2,12 +2,24 @@
 
 namespace FluxErp\Rulesets\AttributeTranslation;
 
+use FluxErp\Models\AttributeTranslation;
+use FluxErp\Rules\ModelExists;
 use FluxErp\Rulesets\FluxRuleset;
 
 class UpdateAttributeTranslationRuleset extends FluxRuleset
 {
     public function rules(): array
     {
-        return [];
+        return [
+            'id' => [
+                'required',
+                'integer',
+                app(ModelExists::class, ['model' => AttributeTranslation::class]),
+            ],
+            'value' => [
+                'required',
+                'string',
+            ],
+        ];
     }
 }
