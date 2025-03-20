59a7408c600a9b2e1ed64b432e8d6d93c03738d8
patrickweh <pw@team-nifty.com>
2025-03-20 10:48:44 +0100

    prettier

--- resources/views/livewire/product/product.blade.php
+++ resources/views/livewire/product/product.blade.php
@@ -37,7 +37,7 @@
         <div
             class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-3 sm:space-y-0 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3"
         >
-           <div>
+            <div>
                 <x-select.styled
                     wire:model="languageId"
                     required
