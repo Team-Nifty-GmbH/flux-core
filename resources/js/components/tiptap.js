2088abba98b75028cddf64325e819d06ac19e697
Tibor <tiborice@gmail.com>
2025-03-24 16:03:16 +0100

    fall back to default text-color on transparent

--- resources/js/components/tiptap.js
+++ resources/js/components/tiptap.js
@@ -55,8 +55,8 @@ export default function (
                     editable: this.editable,
                     editorProps: {
                         attributes: {
-                            class: `${isTransparent ? 'bg-transparent text-black' : 'dark:bg-secondary-800 dark:text-gray-50'} ${showTooltipDropdown ? 'rounded-md' : 'rounded-b-md'} \
-                                prose prose-sm dark:prose-invert max-w-full content-editable-placeholder placeholder-secondary-400 dark:placeholder-secondary-500 \
+                            class: `${isTransparent ? 'bg-transparent' : 'dark:bg-secondary-800'} ${showTooltipDropdown ? 'rounded-md' : 'rounded-b-md'} \
+                                prose prose-sm dark:prose-invert max-w-full content-editable-placeholder dark:text-gray-50 placeholder-secondary-400 dark:placeholder-secondary-500 \
                                 border-secondary-300 focus:ring-primary-500 focus:border-primary-500 dark:border-secondary-600 form-input block \
                                 min-h-[85px] w-full border p-3 shadow-sm transition duration-100 ease-in-out focus:outline-none sm:text-sm`,
                             style: `${initFontSize !== null ? `font-size:${initFontSize}px` : ''}`,
