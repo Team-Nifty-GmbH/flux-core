510b6da2651e1f5160ff60db1b29149f0e22ade4
Steffen Franz <der.wichtel@yahoo.de>
2025-04-09 18:27:56 +0200

    add currency input component

---
+++ resources/views/components/input/currency.blade.php
@@ -0,0 +1,35 @@
+<div
+    x-data="{
+        input: @entangle($attributes->wire('model')),
+        thousands: '{{ $thousands }}',
+        decimal: '{{ $decimal }}',
+        precision: {{ $precision }},
+        maskedValue: null,
+        mask(value) {
+            return (value).replace('.', this.decimal);
+        },
+        emitInput(value) {
+            this.input = value.replaceAll(this.thousands, '').replace(this.decimal, '.');
+        }
+    }"
+    x-init="
+        $watch('input', (value) => {
+            this.maskedValue = mask(value);
+        });
+    "
+>
+    <x-input
+        :label="$label"
+        :hint="$hint"
+        :icon="$icon"
+        :clearable="$clearable"
+        :invalidate="$invalidate"
+        :position="$position"
+        :prefix="$prefix"
+        :suffix="$suffix"
+        x-model="maskedValue"
+        x-mask:dynamic="$money($input, decimal, thousands, precision)"
+        x-init="maskedValue = mask(input)"
+        x-on:blur="emitInput($event.target.value)"
+    />
+</div>
