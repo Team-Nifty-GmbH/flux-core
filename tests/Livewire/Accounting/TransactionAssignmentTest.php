cba6d1c5abf7e7504d49dcb4f1d00983351fb4e0
patrickweh <pw@team-nifty.com>
2025-04-10 14:10:41 +0200

    add missing tests

---
+++ tests/Livewire/Accounting/TransactionAssignmentTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Livewire\Accounting;
+
+use FluxErp\Livewire\Accounting\TransactionAssignment;
+use FluxErp\Tests\TestCase;
+use Livewire\Livewire;
+
+class TransactionAssignmentTest extends TestCase
+{
+    protected string $livewireComponent = TransactionAssignment::class;
+
+    public function test_renders_successfully(): void
+    {
+        Livewire::test($this->livewireComponent)
+            ->assertStatus(200);
+    }
+}
