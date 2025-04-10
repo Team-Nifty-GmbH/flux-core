e5aa1ff7802998cfdbaa9be7efda8b844946524d
patrickweh <pw@team-nifty.com>
2025-04-10 17:56:48 +0200

    refactor currency input handling and improve value normalization

--- resources/views/components/currency.blade.php
+++ resources/views/components/currency.blade.php
@@ -1,21 +1,32 @@
 <div
     x-data="{
-        input: {{ $attributes->wire('model')->value ? '$wire.entangle(\'' . $attributes->wire('model')->value . '\')' : ($attributes->whereStartsWith('x-model')->first() ?: 'null') }},
+        input: {{ $attributes->wire('model')->value ? '$wire.$entangle(\'' . $attributes->wire('model')->value . '\')' : ($attributes->whereStartsWith('x-model')->first() ?: 'null') }},
         thousands: '{{ $thousands }}',
         decimal: '{{ $decimal }}',
         maskedValue: null,
         mask(value) {
             return value?.toString().replace('.', this.decimal)
         },
-        emitInput(value) {
-            this.input = value
-                .replaceAll(this.thousands, '')
+        normalizeValue(value) {
+            return parseFloat(
+                value?.toString().replace(/\./g, '').replace(',', '.'),
+            )
+        },
+        emitInput(event) {
+            this.input = event.target.value
+                ?.replaceAll(this.thousands, '')
                 .replace(this.decimal, '.')
         },
     }"
-    x-init="
+    x-init.once="
+        maskedValue = mask(input)
+        $nextTick(() => $refs.input.dispatchEvent(new Event('input')))
+
         $watch('input', (value) => {
-            this.maskedValue = mask(value)
+            if (parseFloat(value) !== normalizeValue(maskedValue)) {
+                maskedValue = mask(value)
+                $nextTick(() => $refs.input.dispatchEvent(new Event('input')))
+            }
         })
     "
 >
@@ -28,9 +39,19 @@
         :position="$position"
         :prefix="$prefix"
         :suffix="$suffix"
-        x-model="maskedValue"
-        x-mask:dynamic="$money($input, decimal, thousands, {{ $precision }})"
-        x-init="maskedValue = mask(input); emitInput(maskedValue);"
-        x-on:input="emitInput($event.target.value)"
+        :disabled="$attributes->get('disabled')"
+        :readonly="$attributes->get('readonly')"
+        {{
+    $attributes
+        ->merge([
+            'x-ref' => 'input',
+            'x-model' => 'maskedValue',
+            'x-mask:dynamic' => '$money($input, decimal, thousands, ' . $precision . ')',
+            'x-on:input' => 'emitInput($event)',
+            'x-on:blur' => $attributes->wire('model')->hasModifier('blur') ? '$wire.$refresh()' : $attributes->get('x-on:blur'),
+            'x-on:change' => $attributes->wire('model')->hasModifier('change') || $attributes->wire('model')->hasModifier('lazy') ? '$wire.$refresh()' : $attributes->get('x-on:change'),
+        ])
+        ->filter(fn (?string $value, string $key) => ! str_starts_with($key, 'wire:model'))
+}}
     />
 </div>
