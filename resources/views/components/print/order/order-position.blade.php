e61aa0b163340489561e8c92c7a5482072d2e338
patrickweh <pw@team-nifty.com>
2025-04-14 09:53:13 +0200

    refactor order-position blade template to use sections for better organization

--- resources/views/components/print/order/order-position.blade.php
+++ resources/views/components/print/order/order-position.blade.php
@@ -1,12 +1,16 @@
 <tbody class="bg-uneven">
     <tr>
+        @section('order-position')
         <td class="pos py-2 pr-8 align-top">
+            @section('order-position.slug-position')
             {{ $position->total_net_price ? $position->slug_position : '' }}
+            @show
         </td>
         <td
             class="py-2 pr-8 align-top"
             style="padding-left: {{ $position->depth * 15 }}px"
         >
+            @section('order-position.alternative-badge')
             @if ($position->is_alternative)
                 <x-badge
                     color="amber"
@@ -16,21 +20,32 @@
                 />
             @endif
 
+            @show
+
+            @section('order-position.name')
             <p class="font-italic text-xs">
                 {{ $position->product_number }}
             </p>
+            @show
+            @section('order-position.name')
             <p class="font-semibold">
                 {{ $position->name }}
             </p>
+            @show
+            @section('order-position.description')
             <div class="prose-xs prose">
                 {!! $position->description !!}
             </div>
+            @show
         </td>
         <td class="py-2 pr-8 text-center align-top">
+            @section('order-postion.amount')
             {{ format_number($position->amount) }}
             {{ data_get($position, 'product.unit.abbreviation') }}
+            @show
         </td>
         <td class="py-2 text-right align-top">
+            @section('order-position.discount')
             @if ($position->total_base_net_price > $position->total_net_price)
                 <div class="whitespace-nowrap text-xs">
                     <div class="line-through">
@@ -42,7 +57,11 @@
                 </div>
             @endif
 
+            @show
+            @section('order-position.total-price')
             {{ $position->total_net_price ? $formatter->formatCurrency($isNet ? $position->total_net_price : $position->total_gross_price, $currency) : null }}
+            @show
         </td>
+        @show
     </tr>
 </tbody>
