fa0a07ce3240582771237dccbeb684f9af744947
ktownmaxi <maximilian.bernhard@mail.team-nifty.com>
2025-04-22 19:11:58 +0200

    Refactor ClientPaymentType seeder to use factory for creating records

--- database/seeders/ClientPaymentTypeTableSeeder.php
+++ database/seeders/ClientPaymentTypeTableSeeder.php
@@ -4,6 +4,7 @@ namespace FluxErp\Database\Seeders;
 
 use FluxErp\Models\Client;
 use FluxErp\Models\PaymentType;
+use FluxErp\Models\Pivots\ClientPaymentType;
 use Illuminate\Database\Seeder;
 
 class ClientPaymentTypeTableSeeder extends Seeder
@@ -11,10 +12,15 @@ class ClientPaymentTypeTableSeeder extends Seeder
     public function run(): void
     {
         $clientIds = Client::query()->get('id');
+        $cutClientIds = $clientIds->random(bcfloor($clientIds->count() * 0.7));
         $paymentTypesIds = PaymentType::query()->get('id');
+        $cutPaymentTypesIds = $paymentTypesIds->random(bcfloor($paymentTypesIds->count() * 0.7));
 
-        foreach ($clientIds as $clientId) {
-            $clientId->paymentTypes()->attach($paymentTypesIds->random(rand(1, 5)));
+        for ($i = 0; $i < 10; $i++) {
+            ClientPaymentType::factory()->create([
+                'client_id' => $cutClientIds->random()->getKey(),
+                'payment_type_id' => $cutPaymentTypesIds->random()->getKey(),
+            ]);
         }
     }
 }
