7aafb5594d204d733071d45d8973243205b02f18
ktownmaxi <maximilian.bernhard@mail.team-nifty.com>
2025-04-23 15:58:51 +0200

    remove unused imports

--- database/seeders/LedgerAccountTableSeeder.php
+++ database/seeders/LedgerAccountTableSeeder.php
@@ -3,14 +3,7 @@
 namespace FluxErp\Database\Seeders;
 
 use FluxErp\Models\Client;
-use FluxErp\Models\Contact;
-use FluxErp\Models\Currency;
 use FluxErp\Models\LedgerAccount;
-use FluxErp\Models\Media;
-use FluxErp\Models\Order;
-use FluxErp\Models\OrderType;
-use FluxErp\Models\PaymentType;
-use FluxErp\Models\PurchaseInvoice;
 use Illuminate\Database\Seeder;
 
 class LedgerAccountTableSeeder extends Seeder
@@ -21,7 +14,7 @@ class LedgerAccountTableSeeder extends Seeder
         $cutClientIds = $clientIds->random(bcfloor($clientIds->count() * 0.75));
 
         LedgerAccount::factory()->count(9)->create([
-            'client_id' => fn() => $cutClientIds->random()->getKey(),
+            'client_id' => fn () => $cutClientIds->random()->getKey(),
         ]);
     }
 }
