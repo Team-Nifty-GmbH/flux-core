907d3356fa931faf79b57e3f7b3bdcd56a6f4e2a
ktownmaxi <maximilian.bernhard@mail.team-nifty.com>
2025-04-23 17:40:03 +0200

    Add ProductSupplierTableSeeder and update FluxSeeder to include it

---
+++ database/seeders/ProductSupplierTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FluxErp\Database\Seeders;
+
+use FluxErp\Models\Contact;
+use FluxErp\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSupplierTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $productIds = Product::query()->get('id');
+        $cutProductIds = $productIds->random(bcfloor($productIds->count() * 0.6));
+        $ContactIds = Contact::query()->get('id');
+        $cutContactIds = $ContactIds->random(bcfloor($ContactIds->count() * 0.6));
+
+        foreach ($cutProductIds as $cutProductId) {
+            $cutProductId->suppliers()->attach(
+                $cutContactIds->random(rand(1, $cutContactIds->count())),
+                [
+                    'manufacturer_product_number' => faker()->ean8(),
+                    'purchase_price' => faker()->randomFloat(2, 1, 1000),
+                ]
+            );
+        }
+    }
+}
