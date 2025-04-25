1293ab1059f8adaf641a0e24834d09e1e523ab53
patrickweh <pw@team-nifty.com>
2025-04-25 11:54:07 +0200

    refactor: enhance price calculation logic and improve code structure

--- src/Actions/OrderPosition/PriceCalculation.php
+++ src/Actions/OrderPosition/PriceCalculation.php
@@ -3,27 +3,33 @@
 namespace FluxErp\Actions\OrderPosition;
 
 use FluxErp\Helpers\PriceHelper;
-use FluxErp\Livewire\Forms\OrderPositionForm;
 use FluxErp\Models\Contact;
-use FluxErp\Models\Order;
 use FluxErp\Models\OrderPosition;
+use FluxErp\Models\Price;
 use FluxErp\Models\PriceList;
-use FluxErp\Models\Product;
 use FluxErp\Models\StockPosting;
 use FluxErp\Models\VatRate;
 use FluxErp\Traits\Makeable;
-use Illuminate\Database\Eloquent\Model;
 
 class PriceCalculation
 {
     use Makeable;
 
-    public function __construct(public OrderPosition $orderPosition, public array $data) {}
+    public Price|float|string|null $price = null;
+
+    public function __construct(public OrderPosition $orderPosition, public array $data)
+    {
+        $this->orderPosition->loadMissing([
+            'product:id,vat_rate_id',
+            'product.vatRate:id,rate_percentage',
+            'product.prices:id,price_list_id,price',
+            'product.prices.priceList:id,is_net',
+        ]);
+    }
 
     public function fill(): void
     {
-        // Return if no price could be calculated
-        $price = $this->data['unit_price'] ?? $this->orderPosition->unit_price ?? null;
+        $this->price = data_get($this->data, 'unit_price') ?? $this->orderPosition->unit_price;
 
         // A subproduct, aka part of a bundle does not have its own prices.
         if ($this->orderPosition->is_bundle_position) {
@@ -43,119 +49,41 @@ class PriceCalculation
             return;
         }
 
-        $product = $this->orderPosition instanceof Model
-            ? $this->orderPosition->product
-            : resolve_static(Product::class, 'query')
-                ->whereKey($this->orderPosition->product_id)
-                ->first();
-
-        $order = $this->orderPosition instanceof Model
-            ? $this->orderPosition->order
-            : resolve_static(Order::class, 'query')
-                ->whereKey($this->orderPosition->order_id)
-                ->first();
-
         $this->orderPosition->vat_rate_percentage = data_get($this->data, 'vat_rate_percentage')
             ?? resolve_static(VatRate::class, 'query')
                 ->whereKey($this->orderPosition->vat_rate_id)
                 ->value('rate_percentage')
-            ?? $product?->vatRate?->rate_percentage;
+            ?? $this->orderPosition->product?->vatRate?->rate_percentage;
 
-        if (is_null($price) && $product) {
-            $priceHelper = PriceHelper::make($product);
-
-            if ($contactId = data_get($this->data, 'contact_id')) {
-                $priceHelper->setContact(resolve_static(Contact::class, 'query')->whereKey($contactId)->first());
-            }
-
-            if ($priceListId = data_get(
-                $this->data,
-                'price_list_id',
-                $this->orderPosition->price_list_id ?? $order->price_list_id
-            )) {
-                $priceHelper->setPriceList(resolve_static(PriceList::class, 'query')->whereKey($priceListId)->first());
-            }
-
-            $productPrice = $priceHelper->price();
-
-            $price = $this->orderPosition->is_net
-                ? $productPrice?->getNet($this->orderPosition->vat_rate_percentage)
-                : $productPrice?->getGross($this->orderPosition->vat_rate_percentage);
+        if (is_null($this->price) && $this->orderPosition->product) {
+            $this->price = $this->fillFromPriceHelper();
         }
 
-        if (is_null($price)) {
+        if (is_null($this->price)) {
             return;
         }
 
-        $this->orderPosition->unit_price = $price;
+        $this->calculateUnitPrice();
+        $this->calculatePurchasePrice();
+        $this->calculateTotalPrices();
+        $this->calculateDiscounts();
+    }
 
-        // Collect & set missing data
-        if ($product) {
-            $this->orderPosition->product_prices = $product->prices()
-                ->get([
-                    'id',
-                    'price_list_id',
-                    'price',
-                ]);
-        }
-
-        // Calculate net and gross unit prices
-        if ($this->orderPosition->is_net) {
-            $this->orderPosition->unit_net_price = $price;
-            $this->orderPosition->unit_gross_price = net_to_gross($price, $this->orderPosition->vat_rate_percentage);
-        } else {
-            $this->orderPosition->unit_gross_price = $price;
-            $this->orderPosition->unit_net_price = gross_to_net($price, $this->orderPosition->vat_rate_percentage);
-        }
-
-        // calculate net and gross base prices
-        $this->orderPosition->total_base_gross_price = bcmul($this->orderPosition->unit_gross_price, $this->orderPosition->amount);
-        $this->orderPosition->total_base_net_price = bcmul($this->orderPosition->unit_net_price, $this->orderPosition->amount);
-        $this->orderPosition->total_gross_price = $this->orderPosition->total_base_gross_price;
-        $this->orderPosition->total_net_price = $this->orderPosition->total_base_net_price;
-
-        // Purchase-price dependent on stock-bookings.
-        if (! $this->orderPosition->purchase_price) {
-            $stockPosting = resolve_static(StockPosting::class, 'query')
-                ->where('product_id', $this->orderPosition->product_id)
-                ->where('warehouse_id', $this->orderPosition->warehouse_id)
-                ->orderByDesc('id')
-                ->first();
-
-            if (! $stockPosting || bcmul(1, $stockPosting->posting) == 0) {
-                $this->orderPosition->purchase_price = 0;
-            } else {
-                $this->orderPosition->purchase_price = bcdiv($stockPosting->purchase_price, $stockPosting->posting);
-            }
-        }
-
-        $discounts = $this->data['discounts'] ?? [];
-        // Finished collecting, start calculating
-
+    protected function calculateDiscounts(): void
+    {
         // 1. Calculate sum before tax.
         $preDiscountedPrice = $this->orderPosition->is_net
             ? $this->orderPosition->total_base_net_price
             : $this->orderPosition->total_base_gross_price;
 
-        if ($preDiscountedPrice == 0) {
+        if (bccomp($preDiscountedPrice, 0) === 0) {
             $this->orderPosition->vat_price = 0;
 
             return;
         }
 
         // 2. Add any discounts.
-        $discountedPrice = $preDiscountedPrice;
-        foreach ($discounts as $discount) {
-            if ($discount['is_percentage']) {
-                $discountedPrice = discount($discountedPrice, $discount['discount']);
-            } else {
-                $discountedPrice = bcsub($discountedPrice, $discount['discount']);
-            }
-        }
-
-        if (! $discounts && ! is_null(data_get($this->data, 'discount_percentage'))) {
-            $discountedPrice = discount($discountedPrice, $this->data['discount_percentage']);
-        }
+        $discountedPrice = $this->getDiscountedPrice($preDiscountedPrice);
 
         $discountedNetPrice = $this->orderPosition->is_net
             ? $discountedPrice
@@ -168,7 +96,7 @@ class PriceCalculation
         $totalDiscountPercentage = diff_percentage($preDiscountedPrice, $discountedPrice);
         $margin = bcsub($discountedNetPrice, bcmul($this->orderPosition->purchase_price, $this->orderPosition->amount));
 
-        $multiplier = $order->orderType->order_type_enum->multiplier();
+        $multiplier = $this->orderPosition->order->orderType->order_type_enum->multiplier();
         $this->orderPosition->margin = bcmul($margin, $multiplier);
         $this->orderPosition->discount_percentage = $totalDiscountPercentage == 0
             ? null
@@ -178,12 +106,131 @@ class PriceCalculation
         $this->orderPosition->total_gross_price = bcmul($discountedGrossPrice, $multiplier);
 
         if (! is_null($this->orderPosition->vat_rate_percentage)) {
-            $this->orderPosition->vat_price = bcsub($this->orderPosition->total_gross_price, $this->orderPosition->total_net_price);
+            $this->orderPosition->vat_price = bcsub(
+                $this->orderPosition->total_gross_price,
+                $this->orderPosition->total_net_price
+            );
         }
 
-        if ($multiplier !== 1) {
-            $this->orderPosition->total_base_gross_price = bcmul($this->orderPosition->total_base_gross_price, $multiplier);
+        if (bccomp($multiplier, 1) !== 0) {
+            $this->orderPosition->total_base_gross_price = bcmul(
+                $this->orderPosition->total_base_gross_price,
+                $multiplier
+            );
             $this->orderPosition->total_base_net_price = bcmul($this->orderPosition->total_base_net_price, $multiplier);
+        }
+    }
+
+    protected function calculatePurchasePrice(): void
+    {
+        if (! is_null($this->orderPosition->purchase_price)) {
+            return;
+        }
+
+        $stockPosting = resolve_static(StockPosting::class, 'query')
+            ->where('product_id', $this->orderPosition->product_id)
+            ->where('warehouse_id', $this->orderPosition->warehouse_id)
+            ->whereNot('posting', 0)
+            ->orderByDesc('id')
+            ->first();
+
+        $productPurchasePrice = $this->orderPosition->product?->prices()
+            ->whereRelation('priceList', 'is_purchase')
+            ->first();
+
+        $purchasePrice = $stockPosting
+            ? bcdiv($stockPosting->purchase_price, $stockPosting->posting)
+            : $productPurchasePrice?->getNet($this->orderPosition->vat_rate_percentage);
+
+        $this->orderPosition->purchase_price = bcmul($purchasePrice ?? 0, $purchasePrice->amount ?? 0);
+    }
+
+    protected function calculateTotalPrices(): void
+    {
+        // calculate net and gross base prices
+        $this->orderPosition->total_base_gross_price = bcmul($this->orderPosition->unit_gross_price, $this->orderPosition->amount);
+        $this->orderPosition->total_base_net_price = bcmul($this->orderPosition->unit_net_price, $this->orderPosition->amount);
+        $this->orderPosition->total_gross_price = $this->orderPosition->total_base_gross_price;
+        $this->orderPosition->total_net_price = $this->orderPosition->total_base_net_price;
+    }
+
+    protected function calculateUnitPrice(): void
+    {
+        $price = $this->getUnitPrice();
+
+        // Collect & set missing data
+        if ($this->orderPosition->product) {
+            $this->orderPosition->product_prices = $this->orderPosition->product->prices()
+                ->get([
+                    'id',
+                    'price_list_id',
+                    'price',
+                ]);
+        }
+
+        $this->orderPosition->unit_price = $price;
+
+        // Calculate net and gross unit prices
+        if ($this->orderPosition->is_net) {
+            $this->orderPosition->unit_net_price = $price;
+            $this->orderPosition->unit_gross_price = net_to_gross($price, $this->orderPosition->vat_rate_percentage);
+        } else {
+            $this->orderPosition->unit_gross_price = $price;
+            $this->orderPosition->unit_net_price = gross_to_net($price, $this->orderPosition->vat_rate_percentage);
+        }
+    }
+
+    protected function fillFromPriceHelper(): ?Price
+    {
+        $priceHelper = PriceHelper::make($this->orderPosition->product);
+
+        if ($contactId = data_get($this->data, 'contact_id')) {
+            $priceHelper
+                ->setContact(resolve_static(Contact::class, 'query')->whereKey($contactId)->first());
+        }
+
+        if ($priceListId = data_get(
+            $this->data,
+            'price_list_id',
+            $this->orderPosition->price_list_id ?? $this->orderPosition->order->price_list_id
+        )) {
+            $priceHelper
+                ->setPriceList(
+                    resolve_static(PriceList::class, 'query')->whereKey($priceListId)->first()
+                );
+        }
+
+        return $priceHelper->price();
+    }
+
+    protected function getDiscountedPrice(string|float $preDiscounted): string|float
+    {
+        $discountedPrice = $preDiscounted;
+        $discounts = data_get($this->data, 'discounts') ?? [];
+
+        foreach ($discounts as $discount) {
+            if ($discount['is_percentage']) {
+                $discountedPrice = discount($discountedPrice, $discount['discount']);
+            } else {
+                $discountedPrice = bcsub($discountedPrice, $discount['discount']);
+            }
+        }
+
+        if (! $discounts && ! is_null(data_get($this->data, 'discount_percentage'))) {
+            $discountedPrice = discount($discountedPrice, $this->data['discount_percentage']);
+        }
+
+        return $discountedPrice;
+    }
+
+    protected function getUnitPrice(): float|string
+    {
+        if ($this->price instanceof Price) {
+            return $this->orderPosition->is_net
+                ? $this->price->getNet($this->orderPosition->vat_rate_percentage)
+                : $this->price->getGross($this->orderPosition->vat_rate_percentage);
+        } else {
+            return $this->price ?? 0;
         }
     }
 }
