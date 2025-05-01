3e5af3c045982a58100fba24e71da998c32b441b
MaximilianBernhard <maximilian.bernhard@mail.team-nifty.com>
2025-05-01 16:17:17 +0200

    refactor to make widget timeframe aware

--- src/Livewire/Widgets/RecurringRevenueForecast.php
+++ src/Livewire/Widgets/RecurringRevenueForecast.php
@@ -3,6 +3,7 @@
 namespace FluxErp\Livewire\Widgets;
 
 use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 use Cron\CronExpression;
 use FluxErp\Models\Pivots\OrderSchedule;
 use FluxErp\Support\Widgets\Charts\BarChart;
@@ -32,38 +33,13 @@ class RecurringRevenueForecast extends BarChart
     #[Renderless]
     public function calculateByTimeFrame(): void
     {
+        $this->skipRender();
         $this->calculateChart();
+        $this->updateData();
     }
 
     public function calculateChart(): void
     {
-        $start = Carbon::now()->startOfMonth();
-        $end = Carbon::now()->addMonths(12)->endOfMonth();
-        $currentMonthIndex = Carbon::now()->month - 1;
-
-        $orderSchedules = resolve_static(OrderSchedule::class, 'query')
-            ->with(['order', 'schedule'])
-            ->get();
-
-        $clientsData = [];
-
-        foreach ($orderSchedules as $orderSchedule) {
-            if ($orderSchedule->schedule->cron_expression) {
-                $order = $orderSchedule->order;
-                $client = $order->client->name ?? __('Unknown');
-
-                $orderValue = $order->total_net_price;
-                $cron = new CronExpression($orderSchedule->schedule->cron_expression);
-                $nextRun = $cron->getNextRunDate($start);
-
-                while ($nextRun <= $end) {
-                    $month = Carbon::parse($nextRun)->monthName;
-                    $clientsData[$client][$month] = bcadd($clientsData[$client][$month] ?? 0, $orderValue);
-                    $nextRun = $cron->getNextRunDate($nextRun);
-                }
-            }
-        }
-
         $months = [
             __('January'),
             __('February'),
@@ -79,17 +55,64 @@ class RecurringRevenueForecast extends BarChart
             __('December'),
         ];
 
-        $orderedMonths = array_merge(
-            array_slice($months, $currentMonthIndex),
-            array_slice($months, 0, $currentMonthIndex)
-        );
+        $range = $this->timeFrame?->getRange();
+
+        if (is_array($range) && count($range) === 2 && $range[0] && $range[1]) {
+            $startDate = Carbon::parse($range[0]);
+            $endDate = Carbon::parse($range[1]);
+        } elseif ($this->start && $this->end) {
+            $startDate = Carbon::parse($this->start);
+            $endDate = Carbon::parse($this->end);
+        } else {
+            $startDate = now();
+            $endDate = now()->addYear();
+        }
+
+        $period = CarbonPeriod::create($startDate, '1 month', $endDate);
+
+        $startDate = $startDate->subSecond(1); // subtract 1 second to avoid issues with getNextRunDate
+
+        $formattedMonths = [];
+        foreach ($period as $date) {
+            $monthIndex = $date->month - 1;
+            $formattedMonths[] = $months[$monthIndex] . ' ' . $date->year;
+        }
+
+        $orderSchedules = resolve_static(OrderSchedule::class, 'query')
+            ->with(['order', 'schedule'])
+            ->get();
+
+        $clientsData = [];
+
+        foreach ($orderSchedules as $orderSchedule) {
+            $lStartDate = $startDate;
+            if ($orderSchedule->schedule->cron_expression && $orderSchedule->schedule->is_active) {
+                if ($orderSchedule->schedule->created_at > $lStartDate) {
+                    $lStartDate = $orderSchedule->schedule->created_at;
+                }
+                $order = $orderSchedule->order;
+                $client = $order->client->name ?? __('Unknown');
+
+                $orderValue = $order->total_net_price;
+                $cron = new CronExpression($orderSchedule->schedule->cron_expression);
+                $nextRun = $cron->getNextRunDate($lStartDate);
+
+                while ($nextRun <= $endDate &&
+                    (! $orderSchedule->schedule->ends_at || $nextRun <= $orderSchedule->schedule->ends_at)) {
+                    $nextRunTime = Carbon::parse($nextRun);
+                    $month = $nextRunTime->monthName . ' ' . $nextRunTime->year;
+                    $clientsData[$client][$month] = bcadd($clientsData[$client][$month] ?? 0, $orderValue);
+                    $nextRun = $cron->getNextRunDate($nextRun);
+                }
+            }
+        }
 
         $series = [];
 
         foreach ($clientsData as $clientName => $monthlyData) {
             $data = [];
 
-            foreach ($orderedMonths as $month) {
+            foreach ($formattedMonths as $month) {
                 $data[] = $monthlyData[$month] ?? 0;
             }
 
@@ -100,7 +123,7 @@ class RecurringRevenueForecast extends BarChart
         }
 
         $this->series = $series;
-        $this->xaxis['categories'] = $orderedMonths;
+        $this->xaxis['categories'] = $formattedMonths;
     }
 
     public function showTitle(): bool
