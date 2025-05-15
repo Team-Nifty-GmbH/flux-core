6cdf53be67bbcfd8fbfac18c5b7698e2021162f1
patrickweh <pw@team-nifty.com>
2025-05-15 11:41:12 +0200

    refactor: remove order_id assignment from TransactionTableSeeder

--- database/seeders/TransactionTableSeeder.php
+++ database/seeders/TransactionTableSeeder.php
@@ -12,12 +12,11 @@ class TransactionTableSeeder extends Seeder
     public function run(): void
     {
         $bankConnections = BankConnection::all(['id', 'currency_id']);
-        $orders = Order::all(['id']);
+
         foreach ($bankConnections as $bankConnection) {
             Transaction::factory(20)->create([
                 'bank_connection_id' => $bankConnection->id,
                 'currency_id' => $bankConnection->currency_id,
-                'order_id' => rand(0, 1) ? $orders->random()->id : null,
             ]);
         }
     }
