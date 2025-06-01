951e044dde3864eb56964f69bb5e97803d992ca3
patrickweh <pw@team-nifty.com>
2025-06-01 11:56:51 +0200

    refactor: update models method to return a Collection

--- src/Console/Commands/PruneCommand.php
+++ src/Console/Commands/PruneCommand.php
@@ -4,10 +4,11 @@ namespace FluxErp\Console\Commands;
 
 use Illuminate\Database\Console\PruneCommand as BasePruneCommand;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Collection;
 
 class PruneCommand extends BasePruneCommand
 {
-    protected function models()
+    protected function models(): Collection
     {
         $models = parent::models();
 
