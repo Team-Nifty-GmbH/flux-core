11069280373f6d7809c11e46c7d583c3b80b3e04
patrickweh <pw@team-nifty.com>
2025-06-06 17:56:17 +0200

    refactor TimeFrameEnum to include end of day calculations for date ranges

--- src/Enums/TimeFrameEnum.php
+++ src/Enums/TimeFrameEnum.php
@@ -16,27 +16,27 @@ enum TimeFrameEnum: string
         return match ($this) {
             TimeFrameEnum::Today => [
                 $now->subDay()->startOfDay(),
-                $now->subDay(),
+                $now->subDay()->endOfDay(),
             ],
             TimeFrameEnum::Yesterday => [
                 $now->subDays(2)->startOfDay(),
-                $now->subDays(2),
+                $now->subDays(2)->endOfDay(),
             ],
             TimeFrameEnum::ThisWeek => [
-                $now->subWeek()->startOfWeek(),
-                $now->subWeek(),
+                $now->subWeek()->startOfWeek()->startOfDay(),
+                $now->subWeek()->endOfweek()->endOfDay(),
             ],
             TimeFrameEnum::ThisMonth => [
-                $now->subMonthWithoutOverflow()->startOfMonth(),
-                $now->subMonthWithoutOverflow(),
+                $now->subMonthWithoutOverflow()->startOfMonth()->startOfDay(),
+                $now->subMonthWithoutOverflow()->endOfMonth()->endOfDay(),
             ],
             TimeFrameEnum::ThisQuarter => [
-                $now->subQuarter()->startOfQuarter(),
-                $now->subQuarter(),
+                $now->subQuarter()->startOfQuarter()->startOfDay(),
+                $now->subQuarter()->endOfQuarter()->endOfDay(),
             ],
             TimeFrameEnum::ThisYear => [
-                $now->subYear()->startOfYear(),
-                $now->subYear(),
+                $now->subYear()->startOfYear()->startOfDay(),
+                $now->subYear()->endOfYear()->endOfDay(),
             ],
             TimeFrameEnum::Custom => null,
         };
