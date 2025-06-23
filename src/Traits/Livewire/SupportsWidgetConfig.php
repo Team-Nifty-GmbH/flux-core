6b02cd3e5382d786b5e5a5c9b50ea63e967439d1
patrickweh <pw@team-nifty.com>
2025-06-23 10:41:25 +0200

    add widget configuration and event handling methods

---
+++ src/Traits/Livewire/SupportsWidgetConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FluxErp\Traits\Livewire;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Fluent;
+use Livewire\Attributes\Locked;
+use Livewire\Mechanisms\ComponentRegistry;
+
+trait SupportsWidgetConfig
+{
+    use EnsureUsedInLivewire;
+
+    #[Locked]
+    public ?array $config = null;
+
+    protected function storeConfig(string $path, array|bool|string|int|float|Arrayable|null $value): void
+    {
+        $widget = auth()
+            ->user()
+            ->widgets()
+            ->where('dashboard_component', app(ComponentRegistry::class)->getClass($this->dashboardComponent))
+            ->where('component_name', $this->getName())
+            ->first();
+
+        if (is_null($widget->config)) {
+            $widget->config = new Fluent();
+        }
+
+        $widget->config->set($path, $value);
+        $widget->save();
+    }
+}
