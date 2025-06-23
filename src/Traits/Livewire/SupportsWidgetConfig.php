1332967e8ee196878c11d2213ff81c8911041b06
patrickweh <pw@team-nifty.com>
2025-06-23 12:18:49 +0200

    add validation for missing widget configuration

--- src/Traits/Livewire/SupportsWidgetConfig.php
+++ src/Traits/Livewire/SupportsWidgetConfig.php
@@ -4,6 +4,7 @@ namespace FluxErp\Traits\Livewire;
 
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Fluent;
+use Illuminate\Validation\ValidationException;
 use Livewire\Attributes\Locked;
 use Livewire\Mechanisms\ComponentRegistry;
 
@@ -19,9 +20,23 @@ trait SupportsWidgetConfig
         $widget = auth()
             ->user()
             ->widgets()
-            ->where('dashboard_component', app(ComponentRegistry::class)->getClass($this->dashboardComponent))
+            ->where(
+                'dashboard_component',
+                app(ComponentRegistry::class)->getClass($this->dashboardComponent)
+            )
             ->where('component_name', $this->getName())
             ->first();
+
+        if (! $widget) {
+            exception_to_notifications(
+                ValidationException::withMessages([
+                    'dashboard_component' => [__('dashboard_component')],
+                ]),
+                $this
+            );
+
+            return;
+        }
 
         if (is_null($widget->config)) {
             $widget->config = new Fluent();
