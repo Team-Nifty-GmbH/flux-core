486f14d0215eace87740c38d29a644b82e4d6fae
MaximilianBernhard <maximilian.bernhard@mail.team-nifty.com>
2025-07-20 14:04:11 +0200

    add Active Leads by Sales Representative widget and expand LeadTableSeeder

--- database/seeders/LeadTableSeeder.php
+++ database/seeders/LeadTableSeeder.php
@@ -4,6 +4,8 @@ namespace FluxErp\Database\Seeders;
 
 use FluxErp\Models\Address;
 use FluxErp\Models\Lead;
+use FluxErp\Models\LeadState;
+use FluxErp\Models\RecordOrigin;
 use FluxErp\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -12,10 +14,15 @@ class LeadTableSeeder extends Seeder
     public function run(): void
     {
         $addresses = Address::all(['id']);
+        $leadStates = LeadState::all(['id']);
+        $leadRecordOrigins = RecordOrigin::all()->where('model_type', morph_alias(Lead::class))->get(['id']);
 
         foreach (User::all(['id']) as $user) {
             Lead::factory()->create([
+                'lead_state_id' => $leadStates->random()->getKey(),
                 'address_id' => $addresses->random()->getKey(),
+                'record_origin_id' => $leadRecordOrigins->random()->getKey(),
+                'recommended_by_address_id' => $addresses->random()->getKey(),
                 'user_id' => $user->getKey(),
             ]);
         }
---
+++ src/Livewire/Widgets/ActiveLeadsBySalesRepresentative.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace FluxErp\Livewire\Widgets;
+
+use FluxErp\Contracts\HasWidgetOptions;
+use FluxErp\Livewire\Dashboard\Dashboard;
+use FluxErp\Livewire\Lead\LeadList;
+use FluxErp\Livewire\Support\Widgets\Charts\BarChart;
+use FluxErp\Models\User;
+use FluxErp\Traits\Livewire\IsTimeFrameAwareWidget;
+use FluxErp\Traits\Widgetable;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Attributes\Js;
+use Livewire\Attributes\Renderless;
+use Livewire\Livewire;
+use TeamNiftyGmbH\DataTable\Helpers\SessionFilter;
+
+class ActiveLeadsBySalesRepresentative extends BarChart implements HasWidgetOptions
+{
+    use IsTimeFrameAwareWidget, Widgetable;
+
+    public ?array $chart = [
+        'type' => 'bar',
+    ];
+
+    public ?array $dataLabels = [
+        'enabled' => true,
+    ];
+
+    public ?array $plotOptions = [
+        'bar' => [
+            'horizontal' => true,
+            'endingShape' => 'rounded',
+            'columnWidth' => '75%',
+        ],
+    ];
+
+    public bool $showTotals = false;
+
+    public static function dashboardComponent(): array|string
+    {
+        return Dashboard::class;
+    }
+
+    #[Renderless]
+    public function calculateByTimeFrame(): void
+    {
+        $this->calculateChart();
+        $this->updateData();
+    }
+
+    #[Renderless]
+    public function calculateChart(): void
+    {
+        // Default colors of apexcharts
+        $colors = [
+            '#2E93fA',
+            '#66DA26',
+            '#546E7A',
+            '#E91E63',
+            '#FF9800',
+        ];
+
+        $start = $this->getStart()->toDateString();
+        $end = $this->getEnd()->toDateString();
+
+        $leadCounts = resolve_static(User::class, 'query')
+            ->withCount([
+                'leads as total' => function (Builder $query) use ($start, $end): void {
+                    $query->whereHas('leadState', function (Builder $query): void {
+                        $query
+                            ->where('is_won', false)
+                            ->where('is_lost', false);
+                    })
+                        ->whereBetween('end', [$start, $end]);
+                },
+            ])
+            ->having('total', '>', 0)
+            ->orderByDesc('total')
+            ->get();
+
+        $i = 0;
+        $this->series = $leadCounts
+            ->map(function (User $user) use (&$i, $colors): array {
+                return [
+                    'id' => $user->getKey(),
+                    'name' => $user->getLabel(),
+                    'color' => $user->color ?? $colors[$i++ % count($colors)],
+                    'data' => [$user->total],
+                ];
+            })
+            ->take(15)
+            ->values()
+            ->all();
+
+        $this->yaxis = [
+            'labels' => ['show' => false],
+        ];
+    }
+
+    #[Js]
+    public function dataLabelsFormatter(): string
+    {
+        return <<<'JS'
+            return opts.w.config.series[opts.seriesIndex].name + ': ' + val;
+        JS;
+    }
+
+    #[Renderless]
+    public function options(): array
+    {
+        return array_map(
+            fn (array $data) => [
+                'label' => data_get($data, 'name'),
+                'method' => 'show',
+                'params' => [
+                    'id' => data_get($data, 'id'),
+                    'name' => data_get($data, 'name'),
+                ],
+            ],
+            $this->series
+        );
+    }
+
+    #[Renderless]
+    public function show(array $params): void
+    {
+        $start = $this->getStart()->toDateString();
+        $end = $this->getEnd()->toDateString();
+
+        SessionFilter::make(
+            Livewire::new(resolve_static(LeadList::class, 'class'))->getCacheKey(),
+            fn (Builder $query) => $query
+                ->where('user_id', data_get($params, 'id'))
+                ->whereBetween('end', [$start, $end])
+                ->whereHas(
+                    'leadState',
+                    fn (Builder $query) => $query
+                        ->where('is_won', false)
+                        ->where('is_lost', false)
+                ),
+            __('Active leads by :user', ['user' => data_get($params, 'name')]) . ' ' .
+            __('between :start and :end', ['start' => $start, 'end' => $end]),
+        )
+            ->store();
+
+        $this->redirectRoute('sales.leads', navigate: true);
+    }
+}
