1418286f183fe9c74ed4d633a921749d21ed164b
patrickweh <pw@team-nifty.com>
2025-07-24 17:07:37 +0200

    refactor: use morph_alias for task model type

--- src/Livewire/Task/Task.php
+++ src/Livewire/Task/Task.php
@@ -74,7 +74,7 @@ class Task extends Component
         try {
             $tag = CreateTag::make([
                 'name' => $name,
-                'type' => app(TaskModel::class)->getMorphClass(),
+                'type' => morph_alias(TaskModel::class),
             ])
                 ->checkPermission()
                 ->validate()
