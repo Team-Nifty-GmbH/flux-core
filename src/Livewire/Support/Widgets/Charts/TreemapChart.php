26ab39829c3bfc19483e08ec0d9bd8e3332adb46
MaximilianBernhard <maximilian.bernhard@mail.team-nifty.com>
2025-07-31 16:06:29 +0200

    add TreemapChart abstract class for charting functionality

---
+++ src/Livewire/Support/Widgets/Charts/TreemapChart.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace FluxErp\Livewire\Support\Widgets\Charts;
+
+use FluxErp\Traits\Widgetable;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+abstract class TreemapChart extends Chart
+{
+    use Widgetable;
+
+    public ?array $chart = [
+        'type' => 'treemap',
+    ];
+
+    public ?array $dataLabels = [
+        'enabled' => true,
+    ];
+
+    public ?array $plotOptions = [
+        'treemap' => [
+            'spacing' => 10,
+            'enableShades' => true,
+            'shadeIntensity' => 0.5,
+            'reverseNegativeShade' => true,
+            'useFillColorAsStroke' => true,
+        ],
+    ];
+
+    public bool $showTotals = false;
+
+    public ?array $stroke = [
+        'show' => true,
+        'width' => 2,
+        'colors' => ['transparent'],
+    ];
+
+    public function render(): View|Factory
+    {
+        return view('flux::livewire.support.widgets.charts.chart');
+    }
+
+    #[Js]
+    public function dataLabelsFormatter(): string
+    {
+        return <<<'JS'
+            return opts.w.config.xaxis.categories[opts.dataPointIndex] + ': ' + val;
+    JS;
+    }
+
+    public function placeholder(): View|Factory
+    {
+        return view('flux::livewire.placeholders.vertical-bar');
+    }
+}
