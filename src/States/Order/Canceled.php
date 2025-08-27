d857f8a0f332f022429ec4060e44aecdcb47ea65
MaximilianBernhard <maximilian.bernhard@mail.team-nifty.com>
2025-08-27 14:33:22 +0200

    added end state property to all endable States
    Signed-off-by: MaximilianBernhard <maximilian.bernhard@mail.team-nifty.com>

--- src/States/Order/Canceled.php
+++ src/States/Order/Canceled.php
@@ -4,6 +4,8 @@ namespace FluxErp\States\Order;
 
 class Canceled extends OrderState
 {
+    public static bool $isEndState = true;
+
     public static $name = 'canceled';
 
     public function color(): string
--- src/States/Task/InProgress.php
+++ src/States/Task/InProgress.php
@@ -4,8 +4,6 @@ namespace FluxErp\States\Task;
 
 class InProgress extends TaskState
 {
-    public static bool $isEndState = false;
-
     public static $name = 'in_progress';
 
     public function color(): string
