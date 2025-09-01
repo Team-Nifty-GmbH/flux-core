e4ba4658ceb0b430d252a4f204617ff0ada5a163
Tibor <tiborice@gmail.com>
2025-09-01 13:59:17 +0200

    adjust CreatePrintLayout action

--- src/Actions/PrintLayout/CreatePrintLayout.php
+++ src/Actions/PrintLayout/CreatePrintLayout.php
@@ -3,6 +3,7 @@
 namespace FluxErp\Actions\PrintLayout;
 
 use FluxErp\Actions\FluxAction;
+use FluxErp\Actions\Media\UploadMedia;
 use FluxErp\Models\PrintLayout;
 use FluxErp\Rulesets\PrintLayout\CreatePrintLayoutRuleset;
 
@@ -21,7 +22,54 @@ class CreatePrintLayout extends FluxAction
 
     public function performAction(): PrintLayout
     {
-        $printLayout = app(PrintLayout::class, ['attributes' => $this->getData()]);
+        $temporaryMedia = $this->getData('temporaryMedia', []);
+
+        // save print layout to obtain its ID for media relation
+        $printLayout = app(PrintLayout::class);
+        $printLayout->fill([
+            'client_id' => $this->getData('client_id'),
+            'name' => $this->getData('name'),
+            'model_type' => $this->getData('model_type'),
+        ]);
+        $printLayout->save();
+        // footer
+        $footer = $this->getData('footer', []);
+        if($footer['temporaryMedia']) {
+            foreach ($footer['temporaryMedia'] as $imagePosition) {
+                $index = array_search($imagePosition['name'], array_map(fn ($item) => $item->getFilename(), $temporaryMedia));
+                if($index !== false) {
+                    // save temporary images to Media
+                    $tempMedia = $temporaryMedia[$index];
+                    $media =  UploadMedia::make([
+                        'media' => $tempMedia,
+                        'model_id' => $printLayout->id,
+                        'model_type' => morph_alias(PrintLayout::class),
+                        'collection_name' => 'print_layout',
+                    ])->checkPermission()
+                        ->validate()
+                        ->execute();
+                    // mutate footer data to match media
+                    unset($imagePosition['name']);
+                    $imagePosition['id'] = $media->id;
+                    $imagePosition['src'] = $media->original_url;
+                    // add media to footer
+                    $footer['media'][] = $imagePosition;
+                } else {
+                    throw new \Error('Temporary image not found in temporary media - mismatch between footer and temporary media');
+                }
+
+            }
+        }
+        // remove meta data regarding position of temporary images
+        unset($footer['temporaryMedia']);
+
+        $printLayout->fill([
+            'margin' => $this->getData('margin', []),
+            'header' => $this->getData('header', []),
+            'first_page_header' => $this->getData('first_page_header', []),
+            'footer' => $footer,
+        ]);
+
         $printLayout->save();
 
         return $printLayout->fresh();
