832bd3236d2fb7e2596413e8ee5f1661b375dc98
patrickweh <pw@team-nifty.com>
2025-09-04 17:13:08 +0200

    fix: update ProjectTaskListTest to use correct project instance

--- tests/Livewire/Project/ProjectTaskListTest.php
+++ tests/Livewire/Project/ProjectTaskListTest.php
@@ -5,9 +5,10 @@ use FluxErp\Models\Project;
 use Livewire\Livewire;
 
 test('renders successfully', function (): void {
-    $this->project = Project::factory()->create([
-        'client_id' => $client->id,
+    $project = Project::factory()->create([
+        'client_id' => $this->dbClient->id,
     ]);
-    Livewire::test(ProjectTaskList::class, ['projectId' => $this->project->id])
+
+    Livewire::test(ProjectTaskList::class, ['projectId' => $project->id])
         ->assertOk();
 });
