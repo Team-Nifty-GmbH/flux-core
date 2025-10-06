bce972e696e1844b6d91fc7e60fbaef722234bf5
patrickweh <pw@team-nifty.com>
2025-10-06 11:03:16 +0200

    show only active users in permissions

--- tests/Livewire/Settings/PermissionsTest.php
+++ tests/Livewire/Settings/PermissionsTest.php
@@ -1,9 +1,19 @@
 <?php
 
 use FluxErp\Livewire\Settings\Permissions;
+use FluxErp\Models\User;
 use Livewire\Livewire;
 
 test('renders successfully', function (): void {
     Livewire::test(Permissions::class)
         ->assertOk();
+});
+
+test('show only active users', function (): void {
+    User::factory(2)->create(['active' => true]);
+    User::factory(1)->create(['active' => false]);
+
+    Livewire::test(Permissions::class)
+        ->assertOk()
+        ->assertCount('users', 2);
 });
