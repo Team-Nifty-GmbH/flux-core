336ff1f01fd14e44aa0f4e912d07f4d2efa6a845
patrickweh <pw@team-nifty.com>
2025-10-06 14:08:33 +0200

    remove test for showing only active users in permissions

--- tests/Livewire/Settings/PermissionsTest.php
+++ tests/Livewire/Settings/PermissionsTest.php
@@ -1,19 +1,9 @@
 <?php
 
 use FluxErp\Livewire\Settings\Permissions;
-use FluxErp\Models\User;
 use Livewire\Livewire;
 
 test('renders successfully', function (): void {
     Livewire::test(Permissions::class)
         ->assertOk();
-});
-
-test('show only active users', function (): void {
-    User::factory(2)->create(['is_active' => true]);
-    User::factory(1)->create(['is_active' => false]);
-
-    Livewire::test(Permissions::class)
-        ->assertOk()
-        ->assertCount('users', 2);
 });
