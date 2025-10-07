14af5fd89db82cc8148db4c029dbfabb6ebad49f
patrickweh <pw@team-nifty.com>
2025-10-07 19:02:25 +0200

    fix: update event detail access to use 'record.id' for consistency

--- resources/views/livewire/product/warehouse-list.blade.php
+++ resources/views/livewire/product/warehouse-list.blade.php
@@ -2,7 +2,7 @@
     <div class="gap-6 lg:flex">
         <div
             class="flex-1"
-            x-on:data-table-row-clicked="$wire.warehouseId = $event.detail.id;"
+            x-on:data-table-row-clicked="$wire.warehouseId = $event.detail.record.id;"
         >
             @include('tall-datatables::livewire.data-table')
         </div>
