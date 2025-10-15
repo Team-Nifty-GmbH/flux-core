116196fa857ba4f1ad0b73d36154940864151056
patrickweh <pw@team-nifty.com>
2025-10-15 13:05:04 +0200

    add Laravel Data service provider and ensure settings directory exists

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,6 +17,7 @@ use Maatwebsite\Excel\ExcelServiceProvider;
 use NotificationChannels\WebPush\WebPushServiceProvider;
 use Orchestra\Testbench\Concerns\CreatesApplication;
 use Spatie\Activitylog\ActivitylogServiceProvider;
+use Spatie\LaravelData\LaravelDataServiceProvider;
 use Spatie\LaravelSettings\LaravelSettingsServiceProvider;
 use Spatie\MediaLibrary\MediaLibraryServiceProvider;
 use Spatie\Permission\PermissionServiceProvider;
@@ -64,14 +65,16 @@ abstract class TestCase extends BaseTestCase
 
     protected function defineEnvironment($app): void
     {
+        if (! is_dir(database_path('settings'))) {
+            mkdir(database_path('settings'));
+        }
+
         $app['config']->set('database.default', 'mysql');
         $app['config']->set('database.connections.mysql.collation', 'utf8mb4_unicode_ci');
         $app['config']->set('flux.install_done', true);
         $app['config']->set('auth.defaults.guard', 'sanctum');
         $app['config']->set('cache.default', 'array');
         $app['config']->set('settings.auto_discover_settings', []);
-        $app['config']->set('settings.discovered_settings_cache_path', null);
-        $app['config']->set('settings.migrations_paths', []);
     }
 
     protected function getPackageAliases($app): array
