e46a317f0b18cc43115ba339a116c6699232865b
patrickweh <pw@team-nifty.com>
2025-10-15 16:30:49 +0200

    refactor: remove unused componentClass method and related code

--- src/Settings/CoreSettings.php
+++ src/Settings/CoreSettings.php
@@ -2,18 +2,11 @@
 
 namespace FluxErp\Settings;
 
-use FluxErp\Livewire\Settings\CoreSettings as CoreSettingsComponent;
-
 class CoreSettings extends FluxSetting
 {
     public bool $install_done;
 
     public ?string $license_key;
-
-    public static function componentClass(): string
-    {
-        return CoreSettingsComponent::class;
-    }
 
     public static function group(): string
     {
--- src/Settings/FluxSetting.php
+++ src/Settings/FluxSetting.php
@@ -2,17 +2,8 @@
 
 namespace FluxErp\Settings;
 
-use Illuminate\Support\Str;
 use Spatie\LaravelSettings\Settings;
 
 abstract class FluxSetting extends Settings
 {
-    abstract public static function componentClass(): string;
-
-    public static function label(): string
-    {
-        return Str::of(class_basename(static::class))
-            ->headline()
-            ->toString();
-    }
 }
