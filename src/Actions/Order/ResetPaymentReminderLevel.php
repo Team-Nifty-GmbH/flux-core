fc5dbb92ecffbb326cef7c886516b59853932324
patrickweh <pw@team-nifty.com>
2025-10-19 09:59:41 +0200

    add validation for payment reminder level reset and update German translations

--- src/Actions/Order/ResetPaymentReminderLevel.php
+++ src/Actions/Order/ResetPaymentReminderLevel.php
@@ -3,8 +3,11 @@
 namespace FluxErp\Actions\Order;
 
 use FluxErp\Actions\FluxAction;
+use FluxErp\Actions\PaymentReminder\DeletePaymentReminder;
 use FluxErp\Models\Order;
+use FluxErp\Models\PaymentReminder;
 use FluxErp\Rulesets\Order\ResetPaymentReminderLevelRuleset;
+use Illuminate\Validation\ValidationException;
 
 class ResetPaymentReminderLevel extends FluxAction
 {
@@ -27,6 +30,33 @@ class ResetPaymentReminderLevel extends FluxAction
         $order->payment_reminder_current_level = $this->getData('payment_reminder_current_level');
         $order->save();
 
+        $order->paymentReminders()
+            ->where('reminder_level', '>=', $order->payment_reminder_current_level)
+            ->get()
+            ->each(function (PaymentReminder $paymentReminder): void {
+                DeletePaymentReminder::make([
+                    'id' => $paymentReminder->getKey(),
+                ])
+                    ->validate()
+                    ->execute();
+            });
+
         return $order->withoutRelations()->fresh();
+    }
+
+    protected function validateData(): void
+    {
+        parent::validateData();
+
+        $currentLevel = resolve_static(Order::class, 'query')
+            ->whereKey($this->getData('id'))
+            ->value('payment_reminder_current_level');
+
+        if ($this->getData('payment_reminder_current_level') > $currentLevel) {
+            throw ValidationException::withMessages([
+                'payment_reminder_current_level' => ['New level must be smaller than current level'],
+            ])
+                ->errorBag('resetPaymentReminderLevel');
+        }
     }
 }
