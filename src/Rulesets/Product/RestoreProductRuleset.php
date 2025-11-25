fd7df53c6b647a1c8f805a0b3af771e5f33e3ecb
patrickweh <pw@team-nifty.com>
2025-11-25 16:38:31 +0100

    PR changes

--- src/Rulesets/Product/RestoreProductRuleset.php
+++ src/Rulesets/Product/RestoreProductRuleset.php
@@ -2,12 +2,16 @@
 
 namespace FluxErp\Rulesets\Product;
 
+use FluxErp\Enums\BundleTypeEnum;
+use FluxErp\Enums\TimeUnitEnum;
 use FluxErp\Models\Media;
 use FluxErp\Models\Product;
 use FluxErp\Models\Unit;
 use FluxErp\Models\VatRate;
+use FluxErp\Rules\ModelDoesntExist;
 use FluxErp\Rules\ModelExists;
 use FluxErp\Rulesets\FluxRuleset;
+use Illuminate\Validation\Rule;
 
 class RestoreProductRuleset extends FluxRuleset
 {
@@ -53,10 +57,58 @@ class RestoreProductRuleset extends FluxRuleset
                 'nullable',
                 app(ModelExists::class, ['model' => Unit::class]),
             ],
-            'product_number' => 'string|nullable',
+            'bundle_type_enum' => [
+                'required_if_accepted:is_bundle',
+                'nullable',
+                Rule::enum(BundleTypeEnum::class),
+            ],
+            'product_number' => [
+                'string',
+                'nullable',
+                app(ModelDoesntExist::class, ['model' => Product::class, 'key' => 'product_number']),
+            ],
             'name' => 'string|max:255',
             'description' => 'string|nullable',
+            'weight_gram' => 'numeric|nullable',
+            'dimension_length_mm' => 'numeric|nullable',
+            'dimension_width_mm' => 'numeric|nullable',
+            'dimension_height_mm' => 'numeric|nullable',
+            'selling_unit' => 'numeric|nullable',
+            'basic_unit' => 'numeric|nullable',
+            'time_unit_enum' => [
+                'nullable',
+                'required_if:is_service,true',
+                Rule::enum(TimeUnitEnum::class),
+            ],
+            'ean' => 'string|nullable',
+            'customs_tariff_number' => 'string|nullable|max:64',
+            'stock' => 'integer|nullable',
+            'min_delivery_time' => 'integer|nullable',
+            'max_delivery_time' => 'integer|nullable',
+            'restock_time' => 'integer|nullable',
+            'purchase_steps' => 'numeric|nullable',
+            'min_purchase' => 'numeric|nullable',
+            'max_purchase' => 'numeric|nullable',
+            'seo_keywords' => 'string|nullable',
+            'search_aliases' => [
+                'array',
+                'nullable',
+            ],
+            'search_aliases.*' => 'string|max:255|distinct',
+            'posting_account' => 'string|max:255|nullable',
+            'warning_stock_amount' => 'numeric|nullable',
             'is_active' => 'boolean',
+            'is_highlight' => 'boolean',
+            'is_bundle' => [
+                'required_with:bundle_type_enum',
+                'required_if:bundle_type_enum,null',
+                'boolean',
+            ],
+            'is_service' => 'boolean',
+            'is_shipping_free' => 'boolean',
+            'has_serial_numbers' => 'boolean',
+            'is_nos' => 'boolean',
+            'is_active_export_to_web_shop' => 'boolean',
         ];
     }
 }
