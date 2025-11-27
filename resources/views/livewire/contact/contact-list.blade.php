8028edc9aa7f6d1cc93dc7e9f952e2b0274fe2ca
patrickweh <pw@team-nifty.com>
2025-11-27 11:30:32 +0100

    add assign agent to contact list

--- resources/views/livewire/contact/contact-list.blade.php
+++ resources/views/livewire/contact/contact-list.blade.php
@@ -1,5 +1,40 @@
 <div>
     @section('modals')
+    @canAction(\FluxErp\Actions\Contact\UpdateContact::class)
+        <x-modal id="assign-agent-modal" persistent>
+            <x-select.styled
+                :label="__('Commission Agent')"
+                wire:model="agentId"
+                select="label:label|value:id"
+                unfiltered
+                :request="[
+                    'url' => route('search', \FluxErp\Models\User::class),
+                    'method' => 'POST',
+                    'params' => [
+                        'with' => 'media',
+                    ],
+                ]"
+            />
+            <x-slot:footer>
+                <x-button
+                    color="secondary"
+                    light
+                    flat
+                    :text="__('Cancel') "
+                    x-on:click="$modalClose('assign-agent-modal')"
+                />
+                <x-button
+                    color="indigo"
+                    :text="__('Assign')"
+                    loading="assignToAgent"
+                    x-bind:disabled="! $wire.agentId"
+                    wire:flux-confirm.type.warning="{{ __('wire:confirm.contact.assign-agent') }}"
+                    wire:click="assignToAgent().then((success) => {if(success) $modalClose('assign-agent-modal');})"
+                />
+            </x-slot>
+        </x-modal>
+    @endcanAction
+
     @canAction(\FluxErp\Actions\Contact\CreateContact::class)
         <x-modal
             id="create-contact-modal"
