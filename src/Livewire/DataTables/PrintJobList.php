183511ce5b5a6dd816d5517bbaf13ef01d8f9f81
Martin Slupski <mail@slupi.com>
2025-11-27 09:22:59 +0100

    Fix Media serialization error in PrintJobList DataTable

--- src/Livewire/DataTables/PrintJobList.php
+++ src/Livewire/DataTables/PrintJobList.php
@@ -3,6 +3,7 @@
 namespace FluxErp\Livewire\DataTables;
 
 use FluxErp\Models\PrintJob;
+use Illuminate\Database\Eloquent\Builder;
 
 class PrintJobList extends BaseDataTable
 {
@@ -16,4 +17,15 @@ class PrintJobList extends BaseDataTable
     ];
 
     protected string $model = PrintJob::class;
+
+    protected function getBuilder(Builder $builder): Builder
+    {
+        return $builder->with([
+            'media' => fn ($q) => $q->select([
+                'id',
+                'name',
+                'disk',
+            ]),
+        ]);
+    }
 }
