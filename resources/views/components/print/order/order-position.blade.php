9a184c7525b56461e37b40c46df46fccbf152d0c
Maximilian Bernhard <maximilian.bernhard@team-nifty.com>
2026-01-04 12:05:25 +0100

    refactor to use slots instead of sections
    Signed-off-by: Maximilian Bernhard <maximilian.bernhard@team-nifty.com>

--- resources/views/components/print/order/order-position.blade.php
+++ resources/views/components/print/order/order-position.blade.php
@@ -1,80 +1,100 @@
 @use(Illuminate\Support\Facades\Blade; use Illuminate\Support\Number)
-
 <tbody class="bg-uneven">
     <tr>
-        @section('position-identifier.wrapper')
-        <td class="pos py-2 pr-8 align-top">
-            @section('position-identifier.content')
-            {{ $position->total_net_price ? $position->slug_position : '' }}
-            @show
-        </td>
-        @show
+        @if (isset($positionIdentifierWrapper))
+            {{ $positionIdentifierWrapper }}
+        @else
+            <td class="pos py-2 pr-8 align-top">
+                @if (isset($positionIdentifierContent))
+                    {{ $positionIdentifierContent }}
+                @else
+                    {{ $position->total_net_price ? $position->slug_position : '' }}
+                @endif
+            </td>
+        @endif
 
-        @section('position-name.wrapper')
-        <td
-            class="py-2 pr-8 align-top"
-            style="padding-left: {{ $position->depth * 15 }}px"
-        >
-            @section('position-name.content')
-            @section('position-name.content.alternative')
-            @if ($position->is_alternative)
-                <x-badge
-                    color="amber"
-                    class="mb-2"
-                    :text="__('Alternative')"
-                    position="right"
-                />
-            @endif
+        @if (isset($positionNameWrapper))
+            {{ $positionNameWrapper }}
+        @else
+            <td
+                class="py-2 pr-8 align-top"
+                style="padding-left: {{ $position->depth * 15 }}px"
+            >
+                @if (isset($positionNameContent))
+                    {{ $positionNameContent }}
+                @else
+                    @if (isset($alternative))
+                        {{ $alternative }}
+                    @elseif ($position->is_alternative)
+                        <x-badge
+                            color="amber"
+                            class="mb-2"
+                            :text="__('Alternative')"
+                            position="right"
+                        />
+                    @endif
 
-            @show
-            @section('position-name.content.product-number')
-            <p class="font-italic text-xs">
-                {{ $position->product_number }}
-            </p>
-            @show
-            @section('position-name.content.name')
-            <p class="font-semibold">
-                {{ render_editor_blade($position->name, ['position' => $position]) }}
-            </p>
-            @show
-            @section('position-name.content.description')
-            <div class="prose-xs">
-                {{ render_editor_blade($position->description, ['position' => $position]) }}
-            </div>
-            @show
-            @show
-        </td>
-        @show
+                    @if (isset($productNumber))
+                        {{ $productNumber }}
+                    @else
+                        <p class="font-italic text-xs">
+                            {{ $position->product_number }}
+                        </p>
+                    @endif
 
-        @section('position-quantity.wrapper')
-        <td class="py-2 pr-8 text-center align-top">
-            @section('position-quantity.content')
-            @if (! $position->is_free_text && ! $position->is_bundle_position)
-                {{ Number::format($position->amount) }}
-                {{ data_get($position, 'product.unit.abbreviation') }}
-            @endif
+                    @if (isset($name))
+                        {{ $name }}
+                    @else
+                        <p class="font-semibold">
+                            {{ render_editor_blade($position->name, ['position' => $position]) }}
+                        </p>
+                    @endif
 
-            @show
-        </td>
-        @show
+                    @if (isset($description))
+                        {{ $description }}
+                    @else
+                        <div class="prose-xs">
+                            {{ render_editor_blade($position->description, ['position' => $position]) }}
+                        </div>
+                    @endif
+                @endif
+            </td>
+        @endif
 
-        @section('position-price.wrapper')
-        <td class="py-2 text-right align-top">
-            @section('position-price.content')
-            @if (bccomp($position->total_base_net_price ?? 0, $position->total_net_price ?? 0, 2) === 1)
-                <div class="whitespace-nowrap text-xs">
-                    <div class="line-through">
-                        {{ Number::currency($isNet ? $position->total_base_net_price : $position->total_base_gross_price) }}
-                    </div>
-                    <div>
-                        -{{ Number::percentage(bcmul(diff_percentage($position->total_base_net_price, $position->total_net_price), 100), maxPrecision: 2) }}
-                    </div>
-                </div>
-            @endif
+        @if (isset($positionQuantityWrapper))
+            {{ $positionQuantityWrapper }}
+        @else
+            <td class="py-2 pr-8 text-center align-top">
+                @if (isset($positionQuantityContent))
+                    {{ $positionQuantityContent }}
+                @elseif (! $position->is_free_text && ! $position->is_bundle_position)
+                    {{ Number::format($position->amount) }}
+                    {{ data_get($position, 'product.unit.abbreviation') }}
+                @endif
+            </td>
+        @endif
 
-            {{ $position->total_net_price ? Number::currency($isNet ? $position->total_net_price : $position->total_gross_price) : null }}
-            @show
-        </td>
-        @show
+        @if (isset($positionPriceWrapper))
+            {{ $positionPriceWrapper }}
+        @else
+            <td class="py-2 text-right align-top">
+                @if (isset($positionPriceContent))
+                    {{ $positionPriceContent }}
+                @else
+                    @if (bccomp($position->total_base_net_price ?? 0, $position->total_net_price ?? 0, 2) === 1)
+                        <div class="whitespace-nowrap text-xs">
+                            <div class="line-through">
+                                {{ Number::currency($isNet ? $position->total_base_net_price : $position->total_base_gross_price) }}
+                            </div>
+                            <div>
+                                -{{ Number::percentage(bcmul(diff_percentage($position->total_base_net_price, $position->total_net_price), 100), maxPrecision: 2) }}
+                            </div>
+                        </div>
+                    @endif
+
+                    {{ $position->total_net_price ? Number::currency($isNet ? $position->total_net_price : $position->total_gross_price) : null }}
+                @endif
+            </td>
+        @endif
     </tr>
 </tbody>
