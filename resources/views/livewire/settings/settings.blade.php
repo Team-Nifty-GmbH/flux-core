73804cfb72cd4312643b603976bde4b521ad6dd0
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2026-02-04 12:58:59 +0100

    Fix settings page mobile visibility (#1379)
    * Fix settings page mobile visibility
    
    Move x-show/x-cloak to a wrapper div so Alpine reliably controls
    visibility. Default showContent to false on mobile so the settings
    list is shown initially instead of an empty content card.
    
    * prettier

--- resources/views/livewire/settings/settings.blade.php
+++ resources/views/livewire/settings/settings.blade.php
@@ -1,6 +1,10 @@
 <div
     class="flex flex-col gap-4 lg:flex-row"
-    x-data="{ showContent: {{ ! is_null($setting) ? 'true' : 'false' }} }"
+    x-data="{
+        showContent:
+            window.innerWidth >= 1024 &&
+            {{ ! is_null($setting) ? 'true' : 'false' }},
+    }"
 >
     <x-card
         wire:ignore
@@ -25,24 +29,26 @@
             <x-slot:nodeIcon></x-slot>
         </x-flux::checkbox-tree>
     </x-card>
-    <x-card x-show="showContent" x-cloak class="lg:!block">
-        <x-slot:header>
-            <div class="flex items-center gap-2">
-                <x-button
-                    icon="arrow-left"
-                    flat
-                    x-on:click="showContent = false"
-                    class="lg:!hidden"
+    <div x-show="showContent" x-cloak class="flex-1 lg:!block">
+        <x-card>
+            <x-slot:header>
+                <div class="flex items-center gap-2">
+                    <x-button
+                        icon="arrow-left"
+                        flat
+                        x-on:click="showContent = false"
+                        class="lg:!hidden"
+                    />
+                    <div x-text="$wire.setting.path"></div>
+                </div>
+            </x-slot>
+            <x-flux::spinner />
+            @if ($settingComponent)
+                <livewire:is
+                    :component="$settingComponent"
+                    :key="$settingComponent"
                 />
-                <div x-text="$wire.setting.path"></div>
-            </div>
-        </x-slot>
-        <x-flux::spinner />
-        @if ($settingComponent)
-            <livewire:is
-                :component="$settingComponent"
-                :key="$settingComponent"
-            />
-        @endif
-    </x-card>
+            @endif
+        </x-card>
+    </div>
 </div>
