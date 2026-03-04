c012c7d4e32c7a54b58c497b893d2979693cf5b1
Steffen Franz <der.wichtel@yahoo.de>
2026-03-04 14:36:24 +0100

    fix migration if no record was found

--- database/migrations/2026_03_03_164734_migrate_customer_number_serial_number_ranges.php
+++ database/migrations/2026_03_03_164734_migrate_customer_number_serial_number_ranges.php
@@ -20,15 +20,17 @@ return new class() extends Migration
 
         // Keep serial_number_range if highest current_number
         // and set tenant_id = null, unique_key = "contact..customer_number."
-        DB::table('serial_number_ranges')
-            ->where('id', array_shift($serialNumberRanges)->id)
-            ->update([
-                'tenant_id' => null,
-                'unique_key' => 'contact..customer_number.',
-            ]);
-        DB::table('serial_number_ranges')
-            ->whereIntegerInRaw('id', array_column($serialNumberRanges, 'id'))
-            ->delete();
+        if ($serialNumberRanges) {
+            DB::table('serial_number_ranges')
+                ->where('id', array_shift($serialNumberRanges)->id)
+                ->update([
+                    'tenant_id' => null,
+                    'unique_key' => 'contact..customer_number.',
+                ]);
+            DB::table('serial_number_ranges')
+                ->whereIntegerInRaw('id', array_column($serialNumberRanges, 'id'))
+                ->delete();
+        }
     }
 
     public function down(): void {}
