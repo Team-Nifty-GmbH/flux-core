eb03dde78086cf106038a4b21c077029067bf338
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2026-03-06 07:59:08 +0100

    Fix value-box widget options for Livewire 4 (#1452)

--- resources/views/livewire/support/widgets/value-box.blade.php
+++ resources/views/livewire/support/widgets/value-box.blade.php
@@ -14,7 +14,7 @@
                         @foreach ($this->options() ?? [] as $option)
                             <x-dropdown.items
                                 :text="data_get($option, 'label')"
-                                wire:click="{{ data_get($option, 'method') }}('{{ data_get($option, 'params') }}')"
+                                wire:click="$wire.call('{{ data_get($option, 'method') }}', @js(data_get($option, 'params')))"
                             />
                         @endforeach
                     </x-dropdown>
