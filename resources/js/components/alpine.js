4f4de67699853c2d08b28135e5fa4572e51c2f59
patrickweh <pw@team-nifty.com>
2026-04-22 12:58:38 +0200

    Fix flux-confirm rendering 'undefined' when no description is provided
    When wire:flux-confirm has no pipe separator, message[0] is undefined
    after shifting the title. This was concatenated into the description
    HTML as the literal string 'undefined'.

--- resources/js/components/alpine.js
+++ resources/js/components/alpine.js
@@ -116,9 +116,7 @@ Livewire.directive('flux-confirm', ({ el, directive, component }) => {
     let message = directive.expression.replaceAll('\\n', '\n').split('|');
     let title = message.shift();
     let description =
-        '<div>' +
-        message[0] +
-        '</div>' +
+        (message[0] ? '<div>' + message[0] + '</div>' : '') +
         (promptAppend ? '<div>' + promptAppend + '</div>' : '');
     let cancelLabel = message[1] ?? 'Cancel';
     let confirmLabel = message[2] ?? 'Confirm';
