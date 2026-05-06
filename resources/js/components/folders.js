26c1f6ffc37c38bb385439fce88e1eed7c44d13f
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2026-05-06 15:39:27 +0200

    Coerce folder tree checked argument to array (#1690)

--- resources/js/components/folders.js
+++ resources/js/components/folders.js
@@ -23,7 +23,7 @@ export default function folders(
     searchAttributes = null,
 ) {
     return {
-        checked: checked,
+        checked: checked ?? [],
         selected: null,
         openFolders: [],
         tree: [],
