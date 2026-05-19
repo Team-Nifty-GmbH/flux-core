6a76b71782c78747429a02ad3c6f205ca1de0642
Steffen Franz <der.wichtel@yahoo.de>
2026-05-19 16:45:16 +0200

    fix failed tests

---
+++ src/Support/Relations/MorphTo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FluxErp\Support\Relations;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo as EloquentMorphTo;
+
+class MorphTo extends EloquentMorphTo
+{
+    public function createModelByType($type): Model
+    {
+        $class = resolve_static(Model::getActualClassNameForMorph($type), 'class');
+
+        return tap(new $class(), function ($instance) {
+            if (! $instance->getConnectionName()) {
+                $instance->setConnection($this->getConnection()->getName());
+            }
+        });
+    }
+}
