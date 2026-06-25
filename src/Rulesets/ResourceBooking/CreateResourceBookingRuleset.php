9c2c3120d71be1ad3bcc9f11cc6ad276f7b73728
patrickweh <pw@team-nifty.com>
2026-06-25 13:06:00 +0200

    feat: add ResourceBooking rulesets

---
+++ src/Rulesets/ResourceBooking/CreateResourceBookingRuleset.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace FluxErp\Rulesets\ResourceBooking;
+
+use FluxErp\Models\Order;
+use FluxErp\Models\Resource;
+use FluxErp\Models\ResourceBooking;
+use FluxErp\Rules\ModelExists;
+use FluxErp\Rules\MorphClassExists;
+use FluxErp\Rules\MorphExists;
+use FluxErp\Rulesets\FluxRuleset;
+
+class CreateResourceBookingRuleset extends FluxRuleset
+{
+    protected static ?string $model = ResourceBooking::class;
+
+    public function rules(): array
+    {
+        return [
+            'uuid' => 'nullable|string|uuid|unique:resource_bookings,uuid',
+            'resource_id' => [
+                'required',
+                'integer',
+                app(ModelExists::class, ['model' => Resource::class]),
+            ],
+            'assignable_type' => [
+                'required_with:assignable_id',
+                'nullable',
+                'string',
+                app(MorphClassExists::class),
+            ],
+            'assignable_id' => [
+                'required_with:assignable_type',
+                'nullable',
+                'integer',
+                app(MorphExists::class, ['modelAttribute' => 'assignable_type']),
+            ],
+            'order_id' => [
+                'nullable',
+                'integer',
+                app(ModelExists::class, ['model' => Order::class]),
+            ],
+            'start' => 'required|date',
+            'end' => 'required|date|after:start',
+            'description' => 'nullable|string',
+        ];
+    }
+}
---
+++ src/Rulesets/ResourceBooking/UpdateResourceBookingRuleset.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace FluxErp\Rulesets\ResourceBooking;
+
+use FluxErp\Models\Order;
+use FluxErp\Models\Resource;
+use FluxErp\Models\ResourceBooking;
+use FluxErp\Rules\ModelExists;
+use FluxErp\Rules\MorphClassExists;
+use FluxErp\Rules\MorphExists;
+use FluxErp\Rulesets\FluxRuleset;
+
+class UpdateResourceBookingRuleset extends FluxRuleset
+{
+    protected static ?string $model = ResourceBooking::class;
+
+    public function rules(): array
+    {
+        return [
+            'id' => [
+                'required',
+                'integer',
+                app(ModelExists::class, ['model' => ResourceBooking::class]),
+            ],
+            'resource_id' => [
+                'sometimes',
+                'required',
+                'integer',
+                app(ModelExists::class, ['model' => Resource::class]),
+            ],
+            'assignable_type' => [
+                'sometimes',
+                'nullable',
+                'string',
+                app(MorphClassExists::class),
+            ],
+            'assignable_id' => [
+                'sometimes',
+                'nullable',
+                'integer',
+                app(MorphExists::class, ['modelAttribute' => 'assignable_type']),
+            ],
+            'order_id' => [
+                'sometimes',
+                'nullable',
+                'integer',
+                app(ModelExists::class, ['model' => Order::class]),
+            ],
+            'start' => 'sometimes|required|date',
+            'end' => 'sometimes|required|date|after:start',
+            'description' => 'sometimes|nullable|string',
+        ];
+    }
+}
