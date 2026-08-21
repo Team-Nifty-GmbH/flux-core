227a46ff94cd826eb2daaeb600f05d80d664ffdd
Martin Slupski <mail@slupi.com>
2026-08-21 11:21:31 +0200

    Report stock and available stock per bin

--- src/Models/WarehouseBin.php
+++ src/Models/WarehouseBin.php
@@ -11,6 +11,7 @@ use FluxErp\Traits\Model\HasUserModification;
 use FluxErp\Traits\Model\HasUuid;
 use FluxErp\Traits\Model\LogsActivity;
 use FluxErp\Traits\Model\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -45,5 +46,11 @@ class WarehouseBin extends FluxModel
     public function warehouse(): BelongsTo
     {
         return $this->belongsTo(Warehouse::class, 'warehouse_id');
+    }
+
+    public function scopeWithBinStock(Builder $query): void
+    {
+        $query->withSum('stockPostings as stock', 'posting')
+            ->withSum('stockPostings as available_stock', 'remaining_stock');
     }
 }
